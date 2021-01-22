7c982b2db3282d2616d37134cef8dd9ec2df426d
Peng Tao <bergwolf@users.noreply.github.com>
2021-01-23 02:04:13 +0800

    example: use standalone nydusd mode (#49)
    --shared-daemon is not working due to API incompatibility.
    Let's use standalone mode for now.
    
    Signed-off-by: Peng Tao <bergwolf@hyper.sh>

--- misc/example/containerd-entrypoint.sh
+++ misc/example/containerd-entrypoint.sh
@@ -6,7 +6,6 @@ if [ "$#" -eq 0 ]; then
         /opt/bin/containerd -c /opt/etc/containerd/config.toml -l debug &
 	/opt/bin/containerd-nydus-grpc --nydusd-path /opt/bin/nydusd \
 		--config-path /opt/etc/nydusd-config.json \
-		--shared-daemon \
 		--log-level debug \
 		--root /var/lib/containerd-test/io.containerd.snapshotter.v1.nydus \
 		--address /run/containerd-test/containerd-nydus-grpc.sock &
