3ea04cb59009f4f2229d73b657e65d219ba2b424
Eric Ren <renzhen.rz@alibaba-inc.com>
2021-01-23 17:33:42 +0800

    Snapshotter: accept configs from ic.Config
    Signed-off-by: Eric Ren <renzhen.rz@alibaba-inc.com>

--- contrib/nydus-snapshotter/config/config.go
+++ contrib/nydus-snapshotter/config/config.go
@@ -7,18 +7,40 @@
 package config
 
 import (
+	"github.com/pkg/errors"
 	"contrib/nydus-snapshotter/pkg/filesystem/nydus"
 )
 
+const defaultNydusDaemonConfigPath string = "/etc/nydus/config.json"
+const defaultNydusdBinaryPath string = "/usr/local/bin/nydusd"
+
 type Config struct {
-	Address              string
-	ConvertVpcRegistry   bool
-	DaemonCfg            nydus.DaemonConfig
-	PublicKeyFile        string
-	RootDir              string
-	ValidateSignature    bool
-	NydusdBinaryPath     string
-	NydusImageBinaryPath string
-	SharedDaemon         bool
+	Address              string             `toml:"-"`
+	ConvertVpcRegistry   bool               `toml:"-"`
+	DaemonCfgPath        string             `toml:"daemon_cfg_path"`
+	DaemonCfg            nydus.DaemonConfig `toml:"-"`
+	PublicKeyFile        string             `toml:"-"`
+	RootDir              string             `toml:"-"`
+	ValidateSignature    bool               `toml:"validate_signature"`
+	NydusdBinaryPath     string             `toml:"nydusd_binary_path"`
+	NydusImageBinaryPath string             `toml:"-"`
+	SharedDaemon         bool               `toml:"shared_daemon"`
 }
 
+func (c *Config) FillupWithDefaults() error {
+	if c.DaemonCfgPath == "" {
+		c.DaemonCfgPath = defaultNydusDaemonConfigPath
+	}
+
+	if c.NydusdBinaryPath == "" {
+		c.NydusdBinaryPath = defaultNydusdBinaryPath
+	}
+
+	var daemonCfg nydus.DaemonConfig
+	if err := nydus.LoadConfig(c.DaemonCfgPath, &daemonCfg); err != nil {
+		return errors.Wrapf(err, "failed to load config file %q", c.DaemonCfgPath)
+	}
+	c.DaemonCfg = daemonCfg;
+
+	return nil
+}
--- contrib/nydus-snapshotter/export/snapshotter/snapshotter.go
+++ contrib/nydus-snapshotter/export/snapshotter/snapshotter.go
@@ -11,20 +11,24 @@ import (
 	"contrib/nydus-snapshotter/snapshot"
 )
 
-const nydusDaemonConfigPath string = "/etc/nydus/config.json"
-const snapshotterRootDir string = "/var/lib/containerd/io.containerd.snapshotter.v1.nydus"
-const nydusdBinaryPath string = "/usr/local/bin/nydusd"
-
 func init() {
 	plugin.Register(&plugin.Registration{
-		Type: plugin.SnapshotPlugin,
-		ID:   "nydus",
+		Type:   plugin.SnapshotPlugin,
+		ID:     "nydus",
+		Config: &config.Config{},
 		InitFn: func(ic *plugin.InitContext) (interface{}, error) {
 			ic.Meta.Platforms = append(ic.Meta.Platforms, platforms.DefaultSpec())
 
-			cfg, err := defaultConfig()
-			if err != nil {
-				return nil, errors.Wrap(err, "failed to configure snapshotter")
+			cfg, ok := ic.Config.(*config.Config)
+			if !ok {
+				return nil, errors.New("invalid nydus snapshotter configuration")
+			}
+
+			if cfg.RootDir == "" {
+				cfg.RootDir = ic.Root
+			}
+			if err := cfg.FillupWithDefaults(); err != nil {
+				return nil, errors.New("failed to fillup nydus configuration with defaults")
 			}
 
 			fs, err := nydus.NewFileSystem(
@@ -52,20 +56,4 @@ func init() {
 				snapshot.AsynchronousRemove)
 		},
 	})
-}
-
-func defaultConfig() (*config.Config, error) {
-	var daemonCfg nydus.DaemonConfig
-	if err := nydus.LoadConfig(nydusDaemonConfigPath, &daemonCfg); err != nil {
-		return nil, errors.Wrapf(err, "failed to load config file %q", nydusDaemonConfigPath)
-	}
-
-	// TODO(renzhen): support passing config file when loading plugin
-	return &config.Config{
-		DaemonCfg:         daemonCfg,
-		RootDir:           snapshotterRootDir,
-		ValidateSignature: false,
-		NydusdBinaryPath:  nydusdBinaryPath,
-		SharedDaemon:      true,
-	}, nil
 }
