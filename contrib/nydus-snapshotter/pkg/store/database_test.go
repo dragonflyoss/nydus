5c7ff9ff22015a09bfd421ebdc563714c91f6309
Eric Ren <renzhen@linux.alibaba.com>
2021-02-14 13:43:54 +0800

    test: add testcases for store/database.go
    Signed-off-by: Eric Ren <renzhen@linux.alibaba.com>

---
+++ contrib/nydus-snapshotter/pkg/store/database_test.go
@@ -0,0 +1,66 @@
+package store
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"gitlab.alipay-inc.com/antsys/nydus-snapshotter/pkg/daemon"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func Test_database(t *testing.T) {
+	rootDir := "testdata/snapshot"
+	err := os.MkdirAll(rootDir, 0755)
+	require.Nil(t, err)
+	defer func() {
+		_ = os.RemoveAll(rootDir)
+	}()
+
+	dbFile := filepath.Join(rootDir, databaseFileName)
+	db, err := NewDatabase(dbFile)
+	require.Nil(t, err)
+
+	ctx := context.TODO()
+	// Add daemons
+	d1 := daemon.Daemon{ID: "d1"}
+	d2 := daemon.Daemon{ID: "d2"}
+	d3 := daemon.Daemon{ID: "d3"}
+	err = db.SaveDaemon(ctx, &d1)
+	require.Nil(t, err)
+	err = db.SaveDaemon(ctx, &d2)
+	require.Nil(t, err)
+	db.SaveDaemon(ctx, &d3)
+	require.Nil(t, err)
+	// duplicate daemon id should fail
+	err = db.SaveDaemon(ctx, &d1)
+	require.Error(t, err)
+
+	// Delete one daemon
+	err = db.DeleteDaemon(ctx, "d2")
+	require.Nil(t, err)
+
+	// Check records
+	ids := make(map[string]string)
+	err = db.WalkDaemons(ctx, func(info *daemon.Daemon) error {
+		ids[info.ID] = ""
+		return nil
+	})
+	_, ok := ids["d1"]
+	require.Equal(t, ok, true)
+	_, ok = ids["d2"]
+	require.Equal(t, ok, false)
+	_, ok = ids["d3"]
+	require.Equal(t, ok, true)
+
+	// Cleanup records
+	err = db.Cleanup(ctx)
+	require.Nil(t, err)
+	ids2 := make([]string, 0)
+	err = db.WalkDaemons(ctx, func(info *daemon.Daemon) error {
+		ids2 = append(ids2, info.ID)
+		return nil
+	})
+	require.Nil(t, err)
+	require.Equal(t, len(ids2), 0)
+}
