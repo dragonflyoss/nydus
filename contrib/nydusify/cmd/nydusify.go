a67dabdbae4000372651c98a8ecbacf78119c851
Yan Song <imeoer@linux.alibaba.com>
2021-03-03 03:05:57 +0000

    Nydusify: make CLI easier to use
    1. Add `--build-cache-tag` option to simplify specifying build-cache
    reference, for example, specify `--build-cache-tag nydus_cache` will
    set build-cache as this:
    
      target: my-registry.com/namespace/repo:nydus
      build-cache: my-registry.com/namespace/repo:nydus_cache
    
    The option conflicts with `--build-cache`.
    
    2. Wrap parse functions for some options to simplify codes;
    
    Signed-off-by: Yan Song <imeoer@linux.alibaba.com>

--- contrib/nydusify/cmd/nydusify.go
+++ contrib/nydusify/cmd/nydusify.go
@@ -8,9 +8,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/containerd/containerd/reference/docker"
@@ -18,8 +20,10 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 
-	"contrib/nydusify/checker"
-	"contrib/nydusify/converter"
+	"contrib/nydusify/pkg/checker"
+	"contrib/nydusify/pkg/converter"
+	"contrib/nydusify/pkg/converter/provider"
+	"contrib/nydusify/pkg/remote"
 )
 
 var versionGitCommit string
@@ -50,6 +54,58 @@ func parseBackendConfig(backendConfigJSON, backendConfigFile string) (string, er
 	return backendConfigJSON, nil
 }
 
+// Add suffix to source image reference as the target
+// image reference, like this:
+// Source: localhost:5000/nginx:latest
+// Target: localhost:5000/nginx:latest-suffix
+func addReferenceSuffix(source, suffix string) (string, error) {
+	named, err := docker.ParseDockerRef(source)
+	if err != nil {
+		return "", fmt.Errorf("Invalid source image reference: %s", err)
+	}
+	if _, ok := named.(docker.Digested); ok {
+		return "", fmt.Errorf("Unsupported digested image reference: %s", named.String())
+	}
+	named = docker.TagNameOnly(named)
+	target := named.String() + suffix
+	return target, nil
+}
+
+func getTargetReference(c *cli.Context) (string, error) {
+	target := c.String("target")
+	targetSuffix := c.String("target-suffix")
+	if target != "" && targetSuffix != "" {
+		return "", fmt.Errorf("--target conflicts with --target-suffix")
+	}
+	if target == "" && targetSuffix == "" {
+		return "", fmt.Errorf("--target or --target-suffix is required")
+	}
+	var err error
+	if targetSuffix != "" {
+		target, err = addReferenceSuffix(c.String("source"), targetSuffix)
+		if err != nil {
+			return "", err
+		}
+	}
+	return target, nil
+}
+
+func getCacheReference(c *cli.Context, target string) (string, error) {
+	cache := c.String("build-cache")
+	cacheTag := c.String("build-cache-tag")
+	if cache != "" && cacheTag != "" {
+		return "", fmt.Errorf("--build-cache conflicts with --build-cache-tag")
+	}
+	if cacheTag != "" {
+		named, err := docker.ParseDockerRef(target)
+		if err != nil {
+			return "", fmt.Errorf("Invalid target image reference: %s", err)
+		}
+		cache = fmt.Sprintf("%s/%s:%s", docker.Domain(named), docker.Path(named), cacheTag)
+	}
+	return cache, nil
+}
+
 func main() {
 	logrus.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp: true,
@@ -63,16 +119,17 @@ func main() {
 		Version: version,
 	}
 
-	fmt.Printf("Version: %s\n", version)
+	logrus.Infof("Version: %s\n", version)
 
 	app.Commands = []*cli.Command{
 		{
 			Name:  "convert",
 			Usage: "Convert source image to nydus image",
 			Flags: []cli.Flag{
+				&cli.StringFlag{Name: "log-level", Value: "info", Usage: "Set log level (panic, fatal, error, warn, info, debug, trace)", EnvVars: []string{"LOG_LEVEL"}},
 				&cli.StringFlag{Name: "source", Required: true, Usage: "Source image reference", EnvVars: []string{"SOURCE"}},
 				&cli.StringFlag{Name: "target", Required: false, Usage: "Target (Nydus) image reference", EnvVars: []string{"TARGET"}},
-				&cli.StringFlag{Name: "target-suffix", Required: false, Usage: "Add suffix to source image reference as target image reference", EnvVars: []string{"TARGET_SUFFIX"}},
+				&cli.StringFlag{Name: "target-suffix", Required: false, Usage: "Add suffix to source image reference as target image reference, conflict with --target", EnvVars: []string{"TARGET_SUFFIX"}},
 
 				&cli.BoolFlag{Name: "source-insecure", Required: false, Usage: "Allow http/insecure source registry communication", EnvVars: []string{"SOURCE_INSECURE"}},
 				&cli.BoolFlag{Name: "target-insecure", Required: false, Usage: "Allow http/insecure target registry communication", EnvVars: []string{"TARGET_INSECURE"}},
@@ -80,39 +137,26 @@ func main() {
 				&cli.StringFlag{Name: "work-dir", Value: "./tmp", Usage: "Work directory path for image conversion", EnvVars: []string{"WORK_DIR"}},
 				&cli.StringFlag{Name: "prefetch-dir", Value: "/", Usage: "Prefetch directory for nydus image, use absolute path of rootfs", EnvVars: []string{"PREFETCH_DIR"}},
 				&cli.StringFlag{Name: "nydus-image", Value: "./nydus-image", Usage: "The nydus-image binary path", EnvVars: []string{"NYDUS_IMAGE"}},
-				&cli.BoolFlag{Name: "multi-platform", Value: false, Usage: "Add manifest index (multiple platforms, OCI & Nydus) for target image", EnvVars: []string{"MULTI_PLATFORM"}},
+				&cli.BoolFlag{Name: "multi-platform", Value: false, Usage: "Merge OCI & Nydus manifest to manifest index for target image, please ensure that OCI manifest already exists in target image", EnvVars: []string{"MULTI_PLATFORM"}},
 				&cli.BoolFlag{Name: "docker-v2-format", Value: false, Usage: "Use docker image manifest v2, schema 2 format", EnvVars: []string{"DOCKER_V2_FORMAT"}},
 				&cli.StringFlag{Name: "backend-type", Value: "registry", Usage: "Specify Nydus blob storage backend type", EnvVars: []string{"BACKEND_TYPE"}},
 				&cli.StringFlag{Name: "backend-config", Value: "", Usage: "Specify Nydus blob storage backend in JSON config string", EnvVars: []string{"BACKEND_CONFIG"}},
 				&cli.StringFlag{Name: "backend-config-file", Value: "", TakesFile: true, Usage: "Specify Nydus blob storage backend config from path", EnvVars: []string{"BACKEND_CONFIG_FILE"}},
 				&cli.StringFlag{Name: "build-cache", Value: "", Usage: "An remote image reference for accelerating nydus image build", EnvVars: []string{"BUILD_CACHE"}},
+				&cli.StringFlag{Name: "build-cache-tag", Value: "", Usage: "Use $target:$build-cache-tag as cache image reference, conflict with --build-cache", EnvVars: []string{"BUILD_CACHE_TAG"}},
 				&cli.BoolFlag{Name: "build-cache-insecure", Required: false, Usage: "Allow http/insecure registry communication of cache image", EnvVars: []string{"BUILD_CACHE_INSECURE"}},
 				&cli.UintFlag{Name: "build-cache-max-records", Value: 200, Usage: "Maximum cache records in cache image", EnvVars: []string{"BUILD_CACHE_MAX_RECORDS"}},
 			},
 			Action: func(c *cli.Context) error {
-				source := c.String("source")
-				target := c.String("target")
-				targetSuffix := c.String("target-suffix")
-				if target != "" && targetSuffix != "" {
-					return fmt.Errorf("--target conflicts with --target-suffix")
+				logLevel, err := logrus.ParseLevel(c.String("log-level"))
+				if err != nil {
+					return err
 				}
-				if target == "" && targetSuffix == "" {
-					return fmt.Errorf("--target or --target-suffix is required")
-				}
-				// Add suffix to source image reference as the target
-				// image reference, like this:
-				// Source: localhost:5000/nginx:latest
-				// Target: localhost:5000/nginx:latest-suffix
-				if targetSuffix != "" {
-					named, err := docker.ParseDockerRef(source)
-					if err != nil {
-						return fmt.Errorf("Invalid source image reference: %s", err)
-					}
-					if _, ok := named.(docker.Digested); ok {
-						return fmt.Errorf("Unsupported digested image reference: %s", named.String())
-					}
-					named = docker.TagNameOnly(named)
-					target = named.String() + targetSuffix
+				logrus.SetLevel(logLevel)
+
+				target, err := getTargetReference(c)
+				if err != nil {
+					return err
 				}
 
 				backendType := c.String("backend-type")
@@ -128,33 +172,71 @@ func main() {
 					return fmt.Errorf("--backend-config or --backend-config-file required")
 				}
 
-				buildCacheMaxRecords := c.Uint("build-cache-max-records")
-				if buildCacheMaxRecords < 1 {
+				var cacheRemote *remote.Remote
+				cache, err := getCacheReference(c, target)
+				if err != nil {
+					return err
+				}
+				if cache != "" {
+					cacheRemote, err = provider.DefaultRemote(cache, c.Bool("build-cache-insecure"))
+					if err != nil {
+						return err
+					}
+				}
+
+				cacheMaxRecords := c.Uint("build-cache-max-records")
+				if cacheMaxRecords < 1 {
 					return fmt.Errorf("--build-cache-max-records should be greater than 0")
 				}
 
-				converter, err := converter.New(converter.Option{
-					Source:         source,
-					Target:         target,
-					SourceInsecure: c.Bool("source-insecure"),
-					TargetInsecure: c.Bool("target-insecure"),
-
-					WorkDir:              c.String("work-dir"),
-					PrefetchDir:          c.String("prefetch-dir"),
-					NydusImagePath:       c.String("nydus-image"),
-					MultiPlatform:        c.Bool("multi-platform"),
-					DockerV2Format:       c.Bool("docker-v2-format"),
-					BackendType:          backendType,
-					BackendConfig:        backendConfig,
-					BuildCache:           c.String("build-cache"),
-					BuildCacheInsecure:   c.Bool("build-cache-insecure"),
-					BuildCacheMaxRecords: c.Uint("build-cache-max-records"),
-				})
+				logger, err := provider.DefaultLogger()
 				if err != nil {
 					return err
 				}
 
-				return converter.Convert()
+				sourceDir := filepath.Join(c.String("work-dir"), "source")
+				if err := os.RemoveAll(sourceDir); err != nil {
+					return err
+				}
+				if err := os.MkdirAll(sourceDir, 0755); err != nil {
+					return err
+				}
+				sourceProvider, err := provider.DefaultSource(c.String("source"), c.Bool("source-insecure"), sourceDir)
+				if err != nil {
+					return err
+				}
+
+				targetRemote, err := provider.DefaultRemote(target, c.Bool("target-insecure"))
+				if err != nil {
+					return err
+				}
+
+				opt := converter.Opt{
+					Logger:         logger,
+					SourceProvider: sourceProvider,
+
+					TargetRemote: targetRemote,
+
+					CacheRemote:     cacheRemote,
+					CacheMaxRecords: cacheMaxRecords,
+
+					WorkDir:        c.String("work-dir"),
+					PrefetchDir:    c.String("prefetch-dir"),
+					NydusImagePath: c.String("nydus-image"),
+					MultiPlatform:  c.Bool("multi-platform"),
+					DockerV2Format: c.Bool("docker-v2-format"),
+					WhiteoutSpec:   "oci",
+
+					BackendType:   backendType,
+					BackendConfig: backendConfig,
+				}
+
+				cvt, err := converter.New(opt)
+				if err != nil {
+					return err
+				}
+
+				return cvt.Convert(context.Background())
 			},
 		},
 		{
