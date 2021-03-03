61cb3536d401d9eddf2355b08f926dfec999ab2f
Yan Song <imeoer@linux.alibaba.com>
2021-03-03 03:18:37 +0000

    Nydusify: adapt test cases for nydusify package
    Use default provider in nydusify to replace remote and source
    accessor in e2e test, add a case to test build cache.
    
    Signed-off-by: Yan Song <imeoer@linux.alibaba.com>

--- contrib/nydusify/tests/e2e_test.go
+++ contrib/nydusify/tests/e2e_test.go
@@ -31,8 +31,13 @@ func TestConvertWithCache(t *testing.T) {
 	nydusify2.Convert(t)
 	nydusify2.Check(t)
 
-	registry.Build(t, "image-from")
-	nydusify3 := NewNydusify(registry, "image-from", "image-from-nydus-1", "cache:v1")
+	registry.Build(t, "image-from-1")
+	nydusify3 := NewNydusify(registry, "image-from-1", "image-from-nydus-1", "cache:v1")
 	nydusify3.Convert(t)
 	nydusify3.Check(t)
+
+	registry.Build(t, "image-from-2")
+	nydusify4 := NewNydusify(registry, "image-from-2", "image-from-nydus-2", "cache:v1")
+	nydusify4.Convert(t)
+	nydusify4.Check(t)
 }
