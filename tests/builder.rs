faa54f9b4e2bf7414ff1aab467e98110817cc328
Yan Song <imeoer@linux.alibaba.com>
2021-03-10 10:20:00 +0000

    Smoke: remove unnecessary result check
    Replace `Result<T>` with `unwrap`, result check is unnecessary
    in rust tests.
    
    Signed-off-by: Yan Song <imeoer@linux.alibaba.com>

--- tests/builder.rs
+++ tests/builder.rs
@@ -4,7 +4,7 @@
 
 use nix::sys::stat::{dev_t, makedev, mknod, Mode, SFlag};
 use std::fs::{self, File};
-use std::io::{Error, ErrorKind, Result, Write};
+use std::io::Write;
 use std::os::unix::fs as unix_fs;
 use std::path::{Path, PathBuf};
 
@@ -25,204 +25,180 @@ pub fn new<'a>(work_dir: &'a PathBuf, whiteout_spec: &'a str) -> Builder<'a> {
 }
 
 impl<'a> Builder<'a> {
-    fn create_dir(&mut self, path: &PathBuf) -> Result<()> {
-        fs::create_dir_all(path)?;
-        Ok(())
+    fn create_dir(&mut self, path: &PathBuf) {
+        fs::create_dir_all(path).unwrap();
     }
 
-    fn create_file(&mut self, path: &PathBuf, data: &[u8]) -> Result<()> {
-        File::create(path)?.write_all(data)?;
-        Ok(())
+    fn create_file(&mut self, path: &PathBuf, data: &[u8]) {
+        File::create(path).unwrap().write_all(data).unwrap();
     }
 
-    fn copy_file(&mut self, src: &PathBuf, dst: &PathBuf) -> Result<u64> {
-        fs::copy(src, dst)
+    fn copy_file(&mut self, src: &PathBuf, dst: &PathBuf) -> u64 {
+        fs::copy(src, dst).unwrap()
     }
 
-    fn create_symlink(&mut self, src: &PathBuf, dst: &PathBuf) -> Result<()> {
-        unix_fs::symlink(src, dst)?;
-        Ok(())
+    fn create_symlink(&mut self, src: &PathBuf, dst: &PathBuf) {
+        unix_fs::symlink(src, dst).unwrap();
     }
 
-    fn create_hardlink(&mut self, src: &PathBuf, dst: &PathBuf) -> Result<()> {
-        fs::hard_link(src, dst)?;
-        Ok(())
+    fn create_hardlink(&mut self, src: &PathBuf, dst: &PathBuf) {
+        fs::hard_link(src, dst).unwrap();
     }
 
-    fn create_large_file(&mut self, path: &PathBuf, size_in_mb: u8) -> Result<()> {
-        let mut file = File::create(path)?;
+    fn create_large_file(&mut self, path: &PathBuf, size_in_mb: u8) {
+        let mut file = File::create(path).unwrap();
 
         for i in 1..size_in_mb + 1 {
             // Write 1MB data
-            file.write_all(&vec![i; 1024 * 1024])?;
+            file.write_all(&vec![i; 1024 * 1024]).unwrap();
         }
-
-        Ok(())
     }
 
-    fn create_whiteout_file(&mut self, path: &PathBuf) -> Result<()> {
+    fn create_whiteout_file(&mut self, path: &PathBuf) {
         match self.whiteout_spec {
             "overlayfs" => {
                 let dev: dev_t = makedev(0, 0);
-                if let Err(nix::Error::Sys(errno)) = mknod(
+                mknod(
                     path.to_str().unwrap(),
                     SFlag::S_IFCHR,
                     Mode::S_IRUSR | Mode::S_IWUSR,
                     dev,
-                ) {
-                    println!("mknod failed: {} {:?}", errno.desc(), path);
-                    return Err(errno.into());
-                }
+                )
+                .expect("mknod failed");
             }
             "oci" => {
                 let file_name = PathBuf::from(format!(
                     ".wh.{}",
                     path.file_name().unwrap().to_str().unwrap()
                 ));
-                self.create_file(&path.parent().unwrap().join(file_name), b"")?;
+                self.create_file(&path.parent().unwrap().join(file_name), b"");
             }
             _ => {
-                return Err(Error::new(ErrorKind::InvalidInput, "invalid whiteout spec"));
+                panic!("invalid whiteout spec");
             }
         }
-
-        Ok(())
     }
 
-    fn create_opaque_entry(&mut self, path: &PathBuf) -> Result<()> {
+    fn create_opaque_entry(&mut self, path: &PathBuf) {
         match self.whiteout_spec {
             "overlayfs" => {
-                self.set_xattr(path, "trusted.overlay.opaque", "y".as_bytes())?;
+                self.set_xattr(path, "trusted.overlay.opaque", "y".as_bytes());
             }
             "oci" => {
-                self.create_file(&path.join(".wh..wh..opq"), b"")?;
+                self.create_file(&path.join(".wh..wh..opq"), b"");
             }
             _ => {
-                return Err(Error::new(ErrorKind::InvalidInput, "invalid whiteout spec"));
+                panic!("invalid whiteout spec");
             }
         }
-
-        Ok(())
     }
 
-    fn create_special_file(&mut self, path: &PathBuf, devtype: &str) -> Result<()> {
+    fn create_special_file(&mut self, path: &PathBuf, devtype: &str) {
         let dev: dev_t = makedev(255, 0);
         let kind = match devtype {
             "char" => SFlag::S_IFCHR,
             "block" => SFlag::S_IFBLK,
             "fifo" => SFlag::S_IFIFO,
             _ => {
-                return Err(Error::new(
-                    ErrorKind::InvalidInput,
-                    "invalid special file type",
-                ))
+                panic!("invalid special file type");
             }
         };
-        if let Err(nix::Error::Sys(errno)) = mknod(
+        mknod(
             path.to_str().unwrap(),
             kind,
             Mode::S_IRUSR | Mode::S_IWUSR,
             dev,
-        ) {
-            println!("create_special_file failed: {} {:?}", errno.desc(), path);
-            return Err(errno.into());
-        }
-
-        Ok(())
+        )
+        .expect("create_special_file failed");
     }
 
-    fn set_xattr(&mut self, path: &PathBuf, key: &str, value: &[u8]) -> Result<()> {
-        xattr::set(path, key, value)?;
-        Ok(())
+    fn set_xattr(&mut self, path: &PathBuf, key: &str, value: &[u8]) {
+        xattr::set(path, key, value).unwrap();
     }
 
-    pub fn make_lower(&mut self) -> Result<()> {
+    pub fn make_lower(&mut self) {
         let dir = self.work_dir.join("lower");
-        self.create_dir(&dir)?;
+        self.create_dir(&dir);
 
-        self.create_file(&dir.join("root-1"), b"lower:root-1")?;
-        self.create_file(&dir.join("root-2"), b"lower:root-2")?;
-        self.create_large_file(&dir.join("root-large"), 13)?;
-        self.copy_file(&dir.join("root-large"), &dir.join("root-large-copy"))?;
+        self.create_file(&dir.join("root-1"), b"lower:root-1");
+        self.create_file(&dir.join("root-2"), b"lower:root-2");
+        self.create_large_file(&dir.join("root-large"), 13);
+        self.copy_file(&dir.join("root-large"), &dir.join("root-large-copy"));
 
-        self.create_dir(&dir.join("sub"))?;
-        self.create_file(&dir.join("sub/sub-1"), b"lower:sub-1")?;
-        self.create_file(&dir.join("sub/sub-2"), b"lower:sub-2")?;
+        self.create_dir(&dir.join("sub"));
+        self.create_file(&dir.join("sub/sub-1"), b"lower:sub-1");
+        self.create_file(&dir.join("sub/sub-2"), b"lower:sub-2");
         self.create_hardlink(
             &dir.join("root-large"),
             &dir.join("sub/sub-root-large-hardlink"),
-        )?;
+        );
         self.create_hardlink(
             &dir.join("root-large-copy"),
             &dir.join("sub/sub-root-large-copy-hardlink"),
-        )?;
+        );
         self.create_hardlink(
             &dir.join("root-large-copy"),
             &dir.join("sub/sub-root-large-copy-hardlink-1"),
-        )?;
+        );
         self.create_symlink(
             &Path::new("../root-large").to_path_buf(),
             &dir.join("sub/sub-root-large-symlink"),
-        )?;
+        );
 
-        self.create_dir(&dir.join("sub/some"))?;
-        self.create_file(&dir.join("sub/some/some-1"), b"lower:some-1")?;
+        self.create_dir(&dir.join("sub/some"));
+        self.create_file(&dir.join("sub/some/some-1"), b"lower:some-1");
 
-        self.create_dir(&dir.join("sub/more"))?;
-        self.create_file(&dir.join("sub/more/more-1"), b"lower:more-1")?;
-        self.create_dir(&dir.join("sub/more/more-sub"))?;
+        self.create_dir(&dir.join("sub/more"));
+        self.create_file(&dir.join("sub/more/more-1"), b"lower:more-1");
+        self.create_dir(&dir.join("sub/more/more-sub"));
         self.create_file(
             &dir.join("sub/more/more-sub/more-sub-1"),
             b"lower:more-sub-1",
-        )?;
+        );
 
         let long_name = &"test-😉-name.".repeat(100)[..255];
-        self.create_file(&dir.join(long_name), b"lower:long-name")?;
+        self.create_file(&dir.join(long_name), b"lower:long-name");
 
         self.set_xattr(
             &dir.join("sub/sub-1"),
             "user.key-foo",
             "value-foo".as_bytes(),
-        )?;
+        );
 
         self.set_xattr(
             &dir.join("sub/sub-1"),
             "user.key-bar",
             "value-bar".as_bytes(),
-        )?;
-
-        Ok(())
+        );
     }
 
-    pub fn make_upper(&mut self) -> Result<()> {
+    pub fn make_upper(&mut self) {
         let dir = self.work_dir.join("upper");
-        self.create_dir(&dir)?;
+        self.create_dir(&dir);
 
-        self.create_whiteout_file(&dir.join("root-large"))?;
-        self.create_whiteout_file(&dir.join("root-2"))?;
+        self.create_whiteout_file(&dir.join("root-large"));
+        self.create_whiteout_file(&dir.join("root-2"));
 
-        self.create_dir(&dir.join("sub"))?;
-        self.create_file(&dir.join("sub/sub-1"), b"upper:sub-1")?;
-        self.create_whiteout_file(&dir.join("sub/some"))?;
-        self.create_whiteout_file(&dir.join("sub/sub-2"))?;
-        self.create_whiteout_file(&dir.join("sub/sub-root-large-copy-hardlink-1"))?;
+        self.create_dir(&dir.join("sub"));
+        self.create_file(&dir.join("sub/sub-1"), b"upper:sub-1");
+        self.create_whiteout_file(&dir.join("sub/some"));
+        self.create_whiteout_file(&dir.join("sub/sub-2"));
+        self.create_whiteout_file(&dir.join("sub/sub-root-large-copy-hardlink-1"));
 
-        self.create_dir(&dir.join("sub/more"))?;
-        self.create_file(&dir.join("sub/more/more-1"), b"upper:more-1")?;
-        self.create_opaque_entry(&dir.join("sub/more"))?;
-        self.create_dir(&dir.join("sub/more/more-sub"))?;
+        self.create_dir(&dir.join("sub/more"));
+        self.create_file(&dir.join("sub/more/more-1"), b"upper:more-1");
+        self.create_opaque_entry(&dir.join("sub/more"));
+        self.create_dir(&dir.join("sub/more/more-sub"));
         self.create_file(
             &dir.join("sub/more/more-sub/more-sub-2"),
             b"upper:more-sub-2",
-        )?;
-
-        Ok(())
+        );
     }
 
-    pub fn build_lower(&mut self, compressor: &str) -> Result<()> {
+    pub fn build_lower(&mut self, compressor: &str) {
         let lower_dir = self.work_dir.join("lower");
 
-        self.create_dir(&self.work_dir.join("blobs"))?;
+        self.create_dir(&self.work_dir.join("blobs"));
 
         exec(
             format!(
@@ -236,12 +212,10 @@ impl<'a> Builder<'a> {
             )
             .as_str(),
             false,
-        )?;
-
-        Ok(())
+        ).unwrap();
     }
 
-    pub fn build_upper(&mut self, compressor: &str) -> Result<()> {
+    pub fn build_upper(&mut self, compressor: &str) {
         let upper_dir = self.work_dir.join("upper");
 
         exec(
@@ -257,12 +231,10 @@ impl<'a> Builder<'a> {
             )
             .as_str(),
             false,
-        )?;
-
-        Ok(())
+        ).unwrap();
     }
 
-    pub fn build_stargz_lower(&mut self) -> Result<()> {
+    pub fn build_stargz_lower(&mut self) {
         exec(
             format!(
                 "{:?} create --source-type stargz_index --bootstrap {:?} --blob-id {} --log-level info {:?}",
@@ -273,12 +245,10 @@ impl<'a> Builder<'a> {
             )
             .as_str(),
             false,
-        )?;
-
-        Ok(())
+        ).unwrap();
     }
 
-    pub fn build_stargz_upper(&mut self) -> Result<()> {
+    pub fn build_stargz_upper(&mut self) {
         exec(
             format!(
                 "{:?} create --source-type stargz_index --parent-bootstrap {:?} --bootstrap {:?} --blob-id {} --log-level info {:?}",
@@ -290,21 +260,19 @@ impl<'a> Builder<'a> {
             )
             .as_str(),
             false,
-        )?;
-
-        Ok(())
+        ).unwrap();
     }
 
-    pub fn build_special_files(&mut self) -> Result<()> {
+    pub fn build_special_files(&mut self) {
         let dir = self.work_dir.join("special_files");
-        self.create_dir(&dir)?;
-        self.create_dir(&self.work_dir.join("blobs"))?;
+        self.create_dir(&dir);
+        self.create_dir(&self.work_dir.join("blobs"));
 
-        self.create_special_file(&dir.join("block-file"), "block")?;
-        self.create_special_file(&dir.join("char-file"), "char")?;
-        self.create_special_file(&dir.join("fifo-file"), "fifo")?;
-        self.create_file(&dir.join("normal-file"), b"")?;
-        self.create_dir(&dir.join("dir"))?;
+        self.create_special_file(&dir.join("block-file"), "block");
+        self.create_special_file(&dir.join("char-file"), "char");
+        self.create_special_file(&dir.join("fifo-file"), "fifo");
+        self.create_file(&dir.join("normal-file"), b"");
+        self.create_dir(&dir.join("dir"));
 
         exec(
             format!(
@@ -318,8 +286,6 @@ impl<'a> Builder<'a> {
             )
             .as_str(),
             false,
-        )?;
-
-        Ok(())
+        ).unwrap();
     }
 }
--- tests/smoke.rs
+++ tests/smoke.rs
@@ -7,34 +7,31 @@ extern crate log;
 mod builder;
 mod nydusd;
 
-use std::io::Result;
 use std::path::PathBuf;
 
 use vmm_sys_util::tempdir::TempDir;
 
-use nydus_utils::{eother, exec, setup_logging};
+use nydus_utils::{exec, setup_logging};
 
 const COMPAT_BOOTSTRAPS: [&str; 2] = [
     "blake3-lz4_block-non_repeatable",
     "sha256-nocompress-repeatable",
 ];
 
-fn check_compact<'a>(work_dir: &'a PathBuf, bootstrap_name: &str, rafs_mode: &str) -> Result<()> {
+fn check_compact<'a>(work_dir: &'a PathBuf, bootstrap_name: &str, rafs_mode: &str) {
     let nydusd = nydusd::new(
         work_dir,
         false,
         false,
-        rafs_mode.parse()?,
+        rafs_mode.parse().unwrap(),
         "api.sock".into(),
         true,
-    )?;
+    );
 
-    nydusd.start(Some(bootstrap_name), "mnt")?;
+    nydusd.start(Some(bootstrap_name), "mnt");
     let result_path = format!("repeatable/{}.result", bootstrap_name);
-    nydusd.check(result_path.as_str(), "mnt")?;
+    nydusd.check(result_path.as_str(), "mnt");
     nydusd.umount("mnt");
-
-    Ok(())
 }
 
 fn test(
@@ -43,7 +40,7 @@ fn test(
     cache_compressed: bool,
     rafs_mode: &str,
     whiteout_spec: &str,
-) -> Result<()> {
+) {
     // std::thread::sleep(std::time::Duration::from_secs(1000));
 
     info!(
@@ -63,7 +60,7 @@ fn test(
         } else {
             format!("{}/", tmp_dir_prefix)
         };
-        TempDir::new_with_prefix(path).map_err(|e| eother!(e))?
+        TempDir::new_with_prefix(path).unwrap()
     };
     let work_dir = tmp_dir.as_path().to_path_buf();
     let lower_texture = "directory/lower.result".to_string();
@@ -73,40 +70,40 @@ fn test(
 
     {
         // Create & build lower rootfs
-        builder.make_lower()?;
-        builder.build_lower(compressor)?;
+        builder.make_lower();
+        builder.build_lower(compressor);
 
         // Mount lower rootfs and check
         let nydusd = nydusd::new(
             &work_dir,
             enable_cache,
             cache_compressed,
-            rafs_mode.parse()?,
+            rafs_mode.parse().unwrap(),
             "api.sock".into(),
             true,
-        )?;
-        nydusd.start(Some("bootstrap-lower"), "mnt")?;
-        nydusd.check(&lower_texture, "mnt")?;
+        );
+        nydusd.start(Some("bootstrap-lower"), "mnt");
+        nydusd.check(&lower_texture, "mnt");
         nydusd.umount("mnt");
     }
 
     // Mount upper rootfs and check
     {
         // Create & build upper rootfs based lower
-        builder.make_upper()?;
-        builder.build_upper(compressor)?;
+        builder.make_upper();
+        builder.build_upper(compressor);
 
         // Mount overlay rootfs and check
         let nydusd = nydusd::new(
             &work_dir,
             enable_cache,
             cache_compressed,
-            rafs_mode.parse()?,
+            rafs_mode.parse().unwrap(),
             "api.sock".into(),
             true,
-        )?;
-        nydusd.start(Some("bootstrap-overlay"), "mnt")?;
-        nydusd.check(&overlay_texture, "mnt")?;
+        );
+        nydusd.start(Some("bootstrap-overlay"), "mnt");
+        nydusd.check(&overlay_texture, "mnt");
         nydusd.umount("mnt");
     }
 
@@ -116,16 +113,14 @@ fn test(
             &work_dir,
             enable_cache,
             cache_compressed,
-            rafs_mode.parse()?,
+            rafs_mode.parse().unwrap(),
             "api.sock".into(),
             true,
-        )?;
-        nydusd.start(Some("bootstrap-overlay"), "mnt")?;
-        nydusd.check(&overlay_texture, "mnt")?;
+        );
+        nydusd.start(Some("bootstrap-overlay"), "mnt");
+        nydusd.check(&overlay_texture, "mnt");
         nydusd.umount("mnt");
     }
-
-    Ok(())
 }
 
 #[test]
@@ -134,126 +129,121 @@ fn integration_test_init() {
 }
 
 #[test]
-fn integration_test_directory_1() -> Result<()> {
+fn integration_test_directory_1() {
     test("lz4_block", true, false, "direct", "oci")
 }
 
 #[test]
-fn integration_test_directory_2() -> Result<()> {
+fn integration_test_directory_2() {
     test("lz4_block", false, false, "direct", "oci")
 }
 
 #[test]
-fn integration_test_directory_3() -> Result<()> {
+fn integration_test_directory_3() {
     test("gzip", false, false, "direct", "oci")
 }
 
 #[test]
-fn integration_test_directory_4() -> Result<()> {
+fn integration_test_directory_4() {
     test("none", true, false, "direct", "oci")
 }
 
 #[test]
-fn integration_test_directory_5() -> Result<()> {
+fn integration_test_directory_5() {
     test("gzip", true, true, "cached", "oci")
 }
 
 #[test]
-fn integration_test_directory_6() -> Result<()> {
+fn integration_test_directory_6() {
     test("none", false, true, "cached", "oci")
 }
 
 #[test]
-fn integration_test_directory_7() -> Result<()> {
+fn integration_test_directory_7() {
     test("lz4_block", false, true, "cached", "oci")
 }
 
 #[test]
-fn integration_test_directory_8() -> Result<()> {
+fn integration_test_directory_8() {
     test("lz4_block", true, true, "cached", "oci")
 }
 
 #[test]
-fn integration_test_directory_9() -> Result<()> {
+fn integration_test_directory_9() {
     test("lz4_block", true, false, "direct", "overlayfs")
 }
 
 #[test]
-fn integration_test_compact() -> Result<()> {
+fn integration_test_compact() {
     info!("\n\n==================== testing run: compact test");
 
-    let tmp_dir = TempDir::new().map_err(|e| eother!(e))?;
+    let tmp_dir = TempDir::new().unwrap();
     let work_dir = tmp_dir.as_path().to_path_buf();
     let _ = exec(
         format!("cp -a tests/texture/repeatable/* {:?}", work_dir).as_str(),
         false,
-    )?;
+    );
 
     for mode in vec!["direct", "cached"].iter() {
         for bs in COMPAT_BOOTSTRAPS.iter() {
-            check_compact(&work_dir, bs, mode)?;
+            check_compact(&work_dir, bs, mode);
         }
     }
-
-    Ok(())
 }
 
 #[test]
-fn integration_test_special_files() -> Result<()> {
+fn integration_test_special_files() {
     info!("\n\n==================== testing run: special file test");
-    let tmp_dir = TempDir::new().map_err(|e| eother!(e))?;
+    let tmp_dir = TempDir::new().unwrap();
     let work_dir = tmp_dir.as_path().to_path_buf();
 
     let mut builder = builder::new(&work_dir, "oci");
 
-    builder.build_special_files()?;
+    builder.build_special_files();
 
     for mode in &["direct", "cached"] {
         let nydusd = nydusd::new(
             &work_dir,
             true,
             true,
-            mode.parse()?,
+            mode.parse().unwrap(),
             "api.sock".into(),
             false,
-        )?;
-        nydusd.start(Some("bootstrap-specialfiles"), "mnt")?;
-        nydusd.check("specialfiles/result", "mnt")?;
+        );
+        nydusd.start(Some("bootstrap-specialfiles"), "mnt");
+        nydusd.check("specialfiles/result", "mnt");
         nydusd.umount("mnt");
     }
-
-    Ok(())
 }
 
 #[test]
-fn integration_test_stargz() -> Result<()> {
+fn integration_test_stargz() {
     info!("\n\n==================== testing run: stargz test");
 
-    let tmp_dir = TempDir::new().map_err(|e| eother!(e))?;
+    let tmp_dir = TempDir::new().unwrap();
     let work_dir = tmp_dir.as_path().to_path_buf();
 
     let _ = exec(
         format!("cp -a tests/texture/stargz/* {:?}", work_dir).as_str(),
         false,
-    )?;
+    )
+    .unwrap();
 
     let mut builder = builder::new(&work_dir, "oci");
 
-    builder.build_stargz_lower()?;
-    builder.build_stargz_upper()?;
+    builder.build_stargz_lower();
+    builder.build_stargz_upper();
 
     let nydusd = nydusd::new(
         &work_dir,
         true,
         true,
-        "direct".parse()?,
+        "direct".parse().unwrap(),
         "api.sock".into(),
         false,
-    )?;
+    );
 
-    nydusd.start(Some("bootstrap-overlay"), "mnt")?;
-    nydusd.check("directory/overlay.result", "mnt")?;
+    nydusd.start(Some("bootstrap-overlay"), "mnt");
+    nydusd.check("directory/overlay.result", "mnt");
     nydusd.umount("mnt");
-
-    Ok(())
 }
