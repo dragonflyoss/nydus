b68269d73e869bd2d316d723dd7e82e48b038b98
Liu Jiang <gerry@linux.alibaba.com>
2021-08-07 13:18:46 +0800

    utils: replace chrono with httpdate
    The chrono crate has a very big dependency chain, and we only use it
    to format datatime string. So replace it with httpdate to reduce
    building time.
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

--- utils/build.rs
+++ utils/build.rs
@@ -1,3 +1,26 @@
+// Copyright 2020 Ant Group. All rights reserved.
+// Copyright (C) 2021 Alibaba Cloud. All rights reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+use std::io::Write;
+use std::{env, fs, path};
+
 fn main() {
-    built::write_built_file().expect("Failed to acquire build-time information")
+    built::write_built_file().expect("Failed to acquire build-time information");
+
+    let dst = path::Path::new(&env::var("OUT_DIR").unwrap()).join("built.rs");
+    let mut built_file = fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(&dst)
+        .expect("Failed to open file `built.rs`");
+    let datatime = format!(
+        "/// The built-time in RFC2822, UTC\npub const BUILT_TIME_UTC: &str = \"{}\";\n",
+        httpdate::fmt_http_date(std::time::SystemTime::now())
+    );
+
+    built_file
+        .write(datatime.as_bytes())
+        .expect("Failed to write data to `built.rs");
 }
