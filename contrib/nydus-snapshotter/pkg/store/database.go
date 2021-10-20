dragonflyoss/nydus

commit 41671e9d082a1c7c32fe8958ce4327628fc83569

snapshotter: add cache manager to gc blob cache file correctly
GC blob as follows:
1. add snapshot, blob and their relation in boltdb when create container rw snapshot;
2. delete snapshot from boltdb when delete image;
3. delete blob which is not referenced by any snapshot;

Signed-off-by: luodaowen.backend <luodaowen.backend@bytedance.com>

contrib/nydus-snapshotter/pkg/store/database.go
@@ -11,6 +11,7 @@ import (
 	"encoding/json"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/dragonflyoss/image-service/contrib/nydus-snapshotter/pkg/daemon"
 
@@ -18,9 +19,18 @@ import (
 	bolt "go.etcd.io/bbolt"
 )
 
+const (
+	databaseFileName = "nydus.db"
+)
+
 // Bucket names
 var (
 	daemonsBucketName = []byte("daemons") // Contains daemon info <daemon_id>=<daemon>
+
+	cachesBucketName   = []byte("caches")
+	snapshotBucketName = []byte("snapshots")
+
+	blobBucketName = []byte("blobs")
 )
 
 var (
@@ -36,7 +46,8 @@ type Database struct {
 }
 
 // NewDatabase creates a new or open existing database file
-func NewDatabase(dbfile string) (*Database, error) {
+func NewDatabase(rootDir string) (*Database, error) {
+	dbfile := filepath.Join(rootDir, databaseFileName)
 	if err := ensureDirectory(filepath.Dir(dbfile)); err != nil {
 		return nil, err
 	}
@@ -63,6 +74,15 @@ func ensureDirectory(dir string) error {
 func (d *Database) initDatabase() error {
 	return d.db.Update(func(tx *bolt.Tx) error {
 		if _, err := tx.CreateBucketIfNotExists(daemonsBucketName); err != nil {
+			return err
+		}
+		if _, err := tx.CreateBucketIfNotExists(cachesBucketName); err != nil {
+			return err
+		}
+		if _, err := tx.CreateBucketIfNotExists(snapshotBucketName); err != nil {
+			return err
+		}
+		if _, err := tx.CreateBucketIfNotExists(blobBucketName); err != nil {
 			return err
 		}
 		return nil
@@ -141,6 +161,21 @@ func putObject(bucket *bolt.Bucket, key string, obj interface{}) error {
 	return nil
 }
 
+func updateObject(bucket *bolt.Bucket, key string, obj interface{}) error {
+	keyBytes := []byte(key)
+
+	value, err := json.Marshal(obj)
+	if err != nil {
+		return errors.Wrapf(err, "failed to marshall object with key %q", key)
+	}
+
+	if err := bucket.Put(keyBytes, value); err != nil {
+		return errors.Wrapf(err, "failed to insert object with key %q", key)
+	}
+
+	return nil
+}
+
 func getObject(bucket *bolt.Bucket, key string, obj interface{}) error {
 	if obj == nil {
 		return errors.Errorf("invalid arg: obj cannot be nil")
@@ -156,4 +191,125 @@ func getObject(bucket *bolt.Bucket, key string, obj interface{}) error {
 	}
 
 	return nil
+}
+
+func (d *Database) addSnapshot(imageID string, snapshot *Snapshot) error {
+	return d.db.Update(func(tx *bolt.Tx) error {
+		cbkt := tx.Bucket(cachesBucketName)
+		sbkt, err := cbkt.CreateBucketIfNotExists(snapshotBucketName)
+		if err != nil {
+			return err
+		}
+		exist := &Snapshot{}
+
+		if err := getObject(sbkt, imageID, exist); err == nil {
+			exist.Blobs = snapshot.Blobs
+			exist.UpdateAt = time.Now()
+			return updateObject(sbkt, imageID, exist)
+		}
+
+		return putObject(sbkt, imageID, snapshot)
+	})
+}
+
+func (d *Database) addBlob(blobID string, blob *Blob) error {
+	return d.db.Update(func(tx *bolt.Tx) error {
+		cbkt := tx.Bucket(cachesBucketName)
+		bbkt, err := cbkt.CreateBucketIfNotExists(blobBucketName)
+		if err != nil {
+			return err
+		}
+
+		exist := &Blob{}
+		if err := getObject(bbkt, blobID, exist); err == nil {
+			exist.UpdateAt = time.Now()
+			return updateObject(bbkt, blobID, exist)
+		}
+		return putObject(bbkt, blobID, blob)
+	})
+}
+
+func (d *Database) delSnapshot(imageID string) error {
+	return d.db.Update(func(tx *bolt.Tx) error {
+		cbkt := tx.Bucket(cachesBucketName)
+		sbkt, err := cbkt.CreateBucketIfNotExists(snapshotBucketName)
+		if err != nil {
+			return err
+		}
+		if err := sbkt.Delete([]byte(imageID)); err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+func (d *Database) delBlob(blobID string) error {
+	return d.db.Update(func(tx *bolt.Tx) error {
+		cbkt := tx.Bucket(cachesBucketName)
+		bbkt, err := cbkt.CreateBucketIfNotExists(blobBucketName)
+		if err != nil {
+			return err
+		}
+		if err := bbkt.Delete([]byte(blobID)); err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+func (d *Database) getMarked() (map[string]struct{}, error) {
+	var results = make(map[string]struct{})
+	if err := d.db.View(func(tx *bolt.Tx) error {
+		cbkt := tx.Bucket(cachesBucketName)
+		sbkt := cbkt.Bucket(snapshotBucketName)
+
+		return sbkt.ForEach(func(k, v []byte) error {
+			snapshot := &Snapshot{}
+			if err := json.Unmarshal(v, snapshot); err != nil {
+				return err
+			}
+			for _, blobID := range snapshot.Blobs {
+				results[blobID] = struct{}{}
+			}
+			return nil
+		})
+	}); err != nil {
+		return nil, err
+	}
+	return results, nil
+}
+
+func (d *Database) walkBlobs(filter func(blobID string) bool) ([]string, error) {
+	var results []string
+	if err := d.db.View(func(tx *bolt.Tx) error {
+		cbkt := tx.Bucket(cachesBucketName)
+		bbkt := cbkt.Bucket(blobBucketName)
+
+		if err := bbkt.ForEach(func(k, v []byte) error {
+			key := string(k)
+			if filter(key) {
+				results = append(results, key)
+			}
+			return nil
+		}); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return results, nil
+}
+
+func (d *Database) getUnusedBlobs() ([]string, error) {
+	blobSeens, err := d.getMarked()
+	if err != nil {
+		return nil, err
+	}
+	return d.walkBlobs(func(blob string) bool {
+		if _, ok := blobSeens[blob]; !ok {
+			return true
+		}
+		return false
+	})
 }

contrib/nydus-snapshotter/cmd/containerd-nydus-grpc/pkg/command/flags.go
@@ -7,17 +7,21 @@
 package command
 
 import (
+	"os"
+	"path/filepath"
+	"time"
+
 	"github.com/dragonflyoss/image-service/contrib/nydus-snapshotter/config"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
-	"os"
 )
 
 const (
 	defaultAddress        = "/run/containerd-nydus-grpc/containerd-nydus-grpc.sock"
 	defaultLogLevel       = logrus.InfoLevel
 	defaultRootDir        = "/var/lib/containerd-nydus-grpc"
+	defaultGCPeriod       = "24h"
 	defaultPublicKey      = "/signing/nydus-image-signing-public.key"
 	defaultNydusdPath     = "/bin/nydusd"
 	defaultNydusImagePath = "/bin/nydusd-img"
@@ -28,6 +32,8 @@ type Args struct {
 	LogLevel             string
 	ConfigPath           string
 	RootDir              string
+	CacheDir             string
+	GCPeriod             string
 	ValidateSignature    bool
 	PublicKeyFile        string
 	ConvertVpcRegistry   bool
@@ -71,6 +77,18 @@ func buildFlags(args *Args) []cli.Flag {
 			Usage:       "path to the root directory for this snapshotter",
 			Destination: &args.RootDir,
 		},
+		&cli.StringFlag{
+			Name:        "cache-dir",
+			Value:       "",
+			Usage:       "path to the cache dir",
+			Destination: &args.CacheDir,
+		},
+		&cli.StringFlag{
+			Name:        "gc-period",
+			Value:       defaultGCPeriod,
+			Usage:       "period for gc blob cache, for example, 1m, 2h",
+			Destination: &args.GCPeriod,
+		},
 		&cli.BoolFlag{
 			Name:        "validate-signature",
 			Value:       false,
@@ -101,7 +119,7 @@ func buildFlags(args *Args) []cli.Flag {
 			Usage:       "whether automatically convert the image to vpc registry to accelerate image pulling",
 			Destination: &args.ConvertVpcRegistry,
 		},
-	       &cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:        "shared-daemon",
 			Value:       false,
 			Usage:       "Deprecated, equivalent to \"--daemon-mode shared\"",
@@ -160,6 +178,11 @@ func Validate(args *Args, cfg *config.Config) error {
 	}
 	cfg.DaemonCfg = daemonCfg
 	cfg.RootDir = args.RootDir
+
+	cfg.CacheDir = args.CacheDir
+	if len(cfg.CacheDir) == 0 {
+		cfg.CacheDir = filepath.Join(cfg.RootDir, "cache")
+	}
 	cfg.ValidateSignature = args.ValidateSignature
 	cfg.PublicKeyFile = args.PublicKeyFile
 	cfg.ConvertVpcRegistry = args.ConvertVpcRegistry
@@ -175,5 +198,11 @@ func Validate(args *Args, cfg *config.Config) error {
 	cfg.EnableMetrics = args.EnableMetrics
 	cfg.MetricsFile = args.MetricsFile
 	cfg.EnableStargz = args.EnableStargz
+
+	d, err := time.ParseDuration(args.GCPeriod)
+	if err != nil {
+		return errors.Wrapf(err, "parse gc period %v failed", args.GCPeriod)
+	}
+	cfg.GCPeriod = d
 	return nil
 }

contrib/nydus-snapshotter/config/config.go
@@ -7,6 +7,9 @@
 package config
 
 import (
+	"path/filepath"
+	"time"
+
 	"github.com/pkg/errors"
 )
 
@@ -16,6 +19,7 @@ const (
 	DaemonModeShared   string = "shared"
 	DaemonModeSingle   string = "single"
 	DaemonModeNone     string = "none"
+	defaultGCPeriod           = 24 * time.Hour
 
 	defaultNydusDaemonConfigPath string = "/etc/nydus/config.json"
 	defaultNydusdBinaryPath      string = "/usr/local/bin/nydusd"
@@ -23,20 +27,22 @@ const (
 )
 
 type Config struct {
-	Address              string             `toml:"-"`
-	ConvertVpcRegistry   bool               `toml:"-"`
-	DaemonCfgPath        string             `toml:"daemon_cfg_path"`
-	DaemonCfg            DaemonConfig       `toml:"-"`
-	PublicKeyFile        string             `toml:"-"`
-	RootDir              string             `toml:"-"`
-	ValidateSignature    bool               `toml:"validate_signature"`
-	NydusdBinaryPath     string             `toml:"nydusd_binary_path"`
-	NydusImageBinaryPath string             `toml:"nydus_image_binary"`
-	DaemonMode           string             `toml:"daemon_mode"`
-	AsyncRemove          bool               `toml:"async_remove"`
-	EnableMetrics        bool               `toml:"enable_metrics"`
-	MetricsFile          string             `toml:"metrics_file"`
-	EnableStargz         bool               `toml:"enable_stargz"`
+	Address              string        `toml:"-"`
+	ConvertVpcRegistry   bool          `toml:"-"`
+	DaemonCfgPath        string        `toml:"daemon_cfg_path"`
+	DaemonCfg            DaemonConfig  `toml:"-"`
+	PublicKeyFile        string        `toml:"-"`
+	RootDir              string        `toml:"-"`
+	CacheDir             string        `toml:"cache_dir"`
+	GCPeriod             time.Duration `toml:"gc_period"`
+	ValidateSignature    bool          `toml:"validate_signature"`
+	NydusdBinaryPath     string        `toml:"nydusd_binary_path"`
+	NydusImageBinaryPath string        `toml:"nydus_image_binary"`
+	DaemonMode           string        `toml:"daemon_mode"`
+	AsyncRemove          bool          `toml:"async_remove"`
+	EnableMetrics        bool          `toml:"enable_metrics"`
+	MetricsFile          string        `toml:"metrics_file"`
+	EnableStargz         bool          `toml:"enable_stargz"`
 }
 
 func (c *Config) FillupWithDefaults() error {
@@ -56,11 +62,13 @@ func (c *Config) FillupWithDefaults() error {
 		c.DaemonMode = DefaultDaemonMode
 	}
 
+	if len(c.CacheDir) == 0 {
+		c.CacheDir = filepath.Join(c.RootDir, "cache")
+	}
 	var daemonCfg DaemonConfig
 	if err := LoadConfig(c.DaemonCfgPath, &daemonCfg); err != nil {
 		return errors.Wrapf(err, "failed to load config file %q", c.DaemonCfgPath)
 	}
 	c.DaemonCfg = daemonCfg
-
 	return nil
 }

contrib/nydus-snapshotter/go.mod
@@ -3,21 +3,22 @@ module github.com/dragonflyoss/image-service/contrib/nydus-snapshotter
 go 1.14
 
 require (
-	github.com/Microsoft/hcsshim v0.8.9 // indirect
-	github.com/containerd/containerd v1.4.0-beta.1
-	github.com/containerd/continuity v0.0.0-20190426062206-aaeac12a7ffc
+	github.com/containerd/containerd v1.4.3
+	github.com/containerd/continuity v0.0.0-20200928162600-f2cc35102c2a
 	github.com/docker/go-events v0.0.0-20190806004212-e31b211e4f1c // indirect
+	github.com/dragonflyoss/image-service/contrib/nydusify v0.0.0-20210518022841-c17fb49cce7c
+	github.com/godbus/dbus v0.0.0-20190422162347-ade71ed3457e // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e
 	github.com/google/go-containerregistry v0.1.2
-	github.com/google/uuid v1.1.1
+	github.com/google/uuid v1.2.0
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.0.0
 	github.com/prometheus/client_model v0.0.0-20190812154241-14fe0d1b01d4
 	github.com/prometheus/common v0.4.1
-	github.com/sirupsen/logrus v1.6.0
+	github.com/sirupsen/logrus v1.7.0
 	github.com/stretchr/testify v1.5.1
-	github.com/urfave/cli/v2 v2.2.0
+	github.com/urfave/cli/v2 v2.3.0
 	go.etcd.io/bbolt v1.3.5
 	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
 	google.golang.org/grpc v1.31.0

contrib/nydus-snapshotter/pkg/cache/db.go
@@ -1,0 +1,13 @@
+package cache
+
+import (
+	"github.com/dragonflyoss/image-service/contrib/nydus-snapshotter/pkg/store"
+)
+
+type DB interface {
+	AddSnapshot(imageID string, blobs []string) error
+	DelSnapshot(imageID string) error
+	GC(delFunc func(blob string) error) ([]string, error)
+}
+
+var _ DB = &store.CacheStore{}

contrib/nydus-snapshotter/pkg/cache/manager.go
@@ -1,0 +1,86 @@
+package cache
+
+import (
+	"time"
+
+	"github.com/containerd/containerd/log"
+	"github.com/dragonflyoss/image-service/contrib/nydus-snapshotter/pkg/store"
+	"github.com/pkg/errors"
+)
+
+type Manager struct {
+	db       DB
+	store    Store
+	cacheDir string
+	period   time.Duration
+	eventCh  chan struct{}
+}
+
+type Opt struct {
+	CacheDir string
+	Period   time.Duration
+	Database *store.Database
+}
+
+func NewManager(opt Opt) (*Manager, error) {
+	db, err := store.NewCacheStore(opt.Database)
+	if err != nil {
+		return nil, err
+	}
+	s := NewStore(opt.CacheDir)
+
+	eventCh := make(chan struct{})
+	m := &Manager{
+		db:       db,
+		store:    s,
+		cacheDir: opt.CacheDir,
+		period:   opt.Period,
+		eventCh:  eventCh,
+	}
+	go m.runGC()
+	log.L.Info("gc goroutine start...")
+	return m, nil
+}
+
+func (m *Manager) CacheDir() string {
+	return m.cacheDir
+}
+
+func (m *Manager) SchedGC() {
+	m.eventCh <- struct{}{}
+}
+
+func (m *Manager) runGC() {
+	tick := time.NewTicker(m.period)
+	defer tick.Stop()
+	for {
+		select {
+		case <-m.eventCh:
+			if err := m.gc(); err != nil {
+				log.L.Infof("[event] cache gc err, %v", err)
+			}
+			tick.Reset(m.period)
+		case <-tick.C:
+			if err := m.gc(); err != nil {
+				log.L.Infof("[tick] cache gc err, %v", err)
+			}
+		}
+	}
+}
+
+func (m *Manager) gc() error {
+	delBlobs, err := m.db.GC(m.store.DelBlob)
+	if err != nil {
+		return errors.Wrapf(err, "cache gc err")
+	}
+	log.L.Debugf("remove %d unused blobs successfully", len(delBlobs))
+	return nil
+}
+
+func (m *Manager) AddSnapshot(imageID string, blobs []string) error {
+	return m.db.AddSnapshot(imageID, blobs)
+}
+
+func (m *Manager) DelSnapshot(imageID string) error {
+	return m.db.DelSnapshot(imageID)
+}

contrib/nydus-snapshotter/pkg/cache/store.go
@@ -1,0 +1,32 @@
+package cache
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
+
+type Store interface {
+	DelBlob(blob string) error
+}
+
+type CacheStore struct {
+	cacheDir string
+}
+
+func NewStore(cacheDir string) *CacheStore {
+	return &CacheStore{cacheDir: cacheDir}
+}
+
+func (cs *CacheStore) DelBlob(blob string) error {
+	blobPath := cs.blobPath(blob)
+	if err := os.Remove(blobPath); err != nil {
+		return errors.Wrapf(err, "remove blob %v err", blobPath)
+	}
+	return nil
+}
+
+func (cs *CacheStore) blobPath(blob string) string {
+	return filepath.Join(cs.cacheDir, blob)
+}

commit 0cbb1b4e2d4d8a6a3c4729b9482a6471f8c06fd5

snapshotter: create snapshots and blobs bucket on init under cache bucket
We created cache bucket in initDatabase() but not the snapshots and
blobs bucket under cache bucket, these buckets are only created when
adding snapshots & blobs into db.

And if we triggered GC before any snapshot has been created & added
into db, snapshots and blobs buckets are not created, and this will
result in null pointer dereference when doing GC.

Fix it by creating snapshots and blobs bucket under cache bucket as
well in initDatabase().

Fixes: 41671e9d082a ("snapshotter: add cache manager to gc blob cache file correctly")
Signed-off-by: Eryu Guan <eguan@linux.alibaba.com>

contrib/nydus-snapshotter/pkg/store/database.go
@@ -76,13 +76,14 @@ func (d *Database) initDatabase() error {
 		if _, err := tx.CreateBucketIfNotExists(daemonsBucketName); err != nil {
 			return err
 		}
-		if _, err := tx.CreateBucketIfNotExists(cachesBucketName); err != nil {
+		cbkt, err := tx.CreateBucketIfNotExists(cachesBucketName)
+		if err != nil {
 			return err
 		}
-		if _, err := tx.CreateBucketIfNotExists(snapshotBucketName); err != nil {
+		if _, err := cbkt.CreateBucketIfNotExists(snapshotBucketName); err != nil {
 			return err
 		}
-		if _, err := tx.CreateBucketIfNotExists(blobBucketName); err != nil {
+		if _, err := cbkt.CreateBucketIfNotExists(blobBucketName); err != nil {
 			return err
 		}
 		return nil