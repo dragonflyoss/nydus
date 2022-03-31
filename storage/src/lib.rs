cnpm/image-service

refactor: port to macos
event-manager is linux specific, it use to trigger fuse session,
http_thread exit. In this refactor impl, wait fuse channel
thread exit then exit the http_thread.

api-server use eventfd as the request notification to trigger
server run. Because of remove event-manger and eventfd, it can
not work. Add new thread named api-server for it do event
loop by self, and refactor api channel to Option.
If api-server receiver none request means for exit.

Signed-off-by: killagu <killa123@126.com>

storage/src/lib.rs
@@ -43,6 +43,7 @@ extern crate serde;
 extern crate bitflags;
 #[macro_use]
 extern crate nydus_error;
+extern crate micro_http;
 
 use std::fmt::{Display, Formatter};
 

dragonflyoss/nydus

storage/src/lib.rs
@@ -45,6 +45,7 @@ extern crate serde;
 extern crate bitflags;
 #[macro_use]
 extern crate nydus_error;
+extern crate dbs_uhttp;
 
 use std::fmt::{Display, Formatter};
 