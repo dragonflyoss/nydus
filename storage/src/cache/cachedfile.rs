82ec20a6a076aa4484fba6c6e718b3c01cccdffb
Liu Jiang <gerry@linux.alibaba.com>
2022-04-06 18:56:26 +0800

    storage: amplify blob read requests to enable batch operation
    Enable batch operation by amplifying blob read requests to a configured
    batch size.
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

--- storage/src/cache/cachedfile.rs
+++ storage/src/cache/cachedfile.rs
@@ -310,19 +310,15 @@ impl BlobObject for FileCacheEntry {
 
     fn fetch_range_compressed(&self, offset: u64, size: u64) -> Result<usize> {
         let meta = self.meta.as_ref().ok_or_else(|| einval!())?;
-        let chunks = meta.get_chunks_compressed(offset, size)?;
+        let chunks = meta.get_chunks_compressed(offset, size, RAFS_DEFAULT_CHUNK_SIZE * 2)?;
         debug_assert!(!chunks.is_empty());
-
         self.do_fetch_chunks(&chunks)
     }
 
     fn fetch_range_uncompressed(&self, offset: u64, size: u64) -> Result<usize> {
         let meta = self.meta.as_ref().ok_or_else(|| einval!())?;
-
-        // TODO: read amplify the range to naturally aligned 2M?
-        let chunks = meta.get_chunks_uncompressed(offset, size)?;
+        let chunks = meta.get_chunks_uncompressed(offset, size, RAFS_DEFAULT_CHUNK_SIZE * 2)?;
         debug_assert!(!chunks.is_empty());
-
         self.do_fetch_chunks(&chunks)
     }
 
