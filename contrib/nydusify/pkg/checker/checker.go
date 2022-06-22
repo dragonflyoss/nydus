7f3ac5b789ba7d3513bf78d26f0cbb9e0b558484
zyfjeff <zyfjeff@linux.alibaba.com>
2022-06-22 11:41:03 +0000

    Get nydus version from bootstrap layer
    Signed-off-by: zyfjeff <zyfjeff@linux.alibaba.com>

--- contrib/nydusify/pkg/checker/checker.go
+++ contrib/nydusify/pkg/checker/checker.go
@@ -109,12 +109,15 @@ func (checker *Checker) Check(ctx context.Context) error {
 	mode := "direct"
 	digestValidate := false
 	if targetParsed.NydusImage != nil {
-		v := utils.GetNydusFsVersionOrDefault(targetParsed.NydusImage.Manifest.Annotations, utils.V5)
-		if v == utils.V5 {
-			// Digest validate is not currently supported for v6,
-			// but v5 supports it. In order to make the check more sufficient,
-			// this validate needs to be turned on for v5.
-			digestValidate = true
+		nydusManifest := parser.FindNydusBootstrapDesc(&targetParsed.NydusImage.Manifest)
+		if nydusManifest != nil {
+			v := utils.GetNydusFsVersionOrDefault(nydusManifest.Annotations, utils.V5)
+			if v == utils.V5 {
+				// Digest validate is not currently supported for v6,
+				// but v5 supports it. In order to make the check more sufficient,
+				// this validate needs to be turned on for v5.
+				digestValidate = true
+			}
 		}
 	}
 
