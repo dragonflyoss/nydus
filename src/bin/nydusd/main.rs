dragonflyoss/nydus

commit c51151eac2f3a5beed367b2ac1060b8a0d4ca675

nydusd: set default vfs opts for rafs and passthroughfs on root inode
For rafs, we would like to disable open.
For passthroughfs, we would like to enable open and killpriv_v2.

Signed-off-by: Peng Tao <bergwolf@hyper.sh>

src/bin/nydusd/main.rs
@@ -301,7 +301,7 @@ fn main() -> Result<()> {
         .map(|n| n.parse().unwrap_or(rlimit_nofile_default))
         .unwrap_or(rlimit_nofile_default);
 
-    let vfs = Vfs::new(VfsOptions::default());
+    let mut opts = VfsOptions::default();
     let mount_cmd = if let Some(shared_dir) = shared_dir {
         info!(
             "set rlimit {}, default {}",
@@ -319,6 +319,10 @@ fn main() -> Result<()> {
             mountpoint: virtual_mnt.to_string(),
             prefetch_files: None,
         };
+
+        // passthroughfs requires !no_open
+        opts.no_open = false;
+        opts.killpriv_v2 = true;
 
         Some(cmd)
     } else if let Some(b) = bootstrap {
@@ -338,10 +342,15 @@ fn main() -> Result<()> {
             prefetch_files,
         };
 
+        // rafs can be readonly and skip open
+        opts.no_open = true;
+
         Some(cmd)
     } else {
         None
     };
+
+    let vfs = Vfs::new(opts);
 
     let mut event_manager = EventManager::<Arc<dyn EventSubscriber>>::new().unwrap();
 

commit 64b3c0cc66b140b7e50526f6c5f417bd63431928

nydusd: disable no_opendir for passthrough_fs
Otherwise, there would be inconsistency between host and guest readdir()
results.

Fixes: c51151eac2f3a5beed367b2ac1060b8a0d4ca675
Reported-by: Daowen Luo <luodaowen.backend@bytedance.com>
Signed-off-by: Jiachen Zhang <zhangjiachen.jaycee@bytedance.com>

src/bin/nydusd/main.rs
@@ -586,6 +586,7 @@ fn process_default_fs_service(
 
         // passthroughfs requires !no_open
         opts.no_open = false;
+        opts.no_opendir = false;
         opts.killpriv_v2 = true;
 
         Some(cmd)
@@ -617,6 +618,7 @@ fn process_default_fs_service(
     // Enable all options required by passthroughfs
     if args.is_present("hybrid-mode") {
         opts.no_open = false;
+        opts.no_opendir = false;
         opts.killpriv_v2 = true;
     }
 