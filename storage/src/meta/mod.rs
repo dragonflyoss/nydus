dragonflyoss/nydus

commit ab5e7c5fe8f1cb2b1087535a225eccfea1001c8a

storage: amplify blob read requests to enable batch operation
Enable batch operation by amplifying blob read requests to a configured
batch size.

Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>
Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>

storage/src/meta/mod.rs
@@ -405,7 +405,12 @@ impl BlobMetaInfo {
     /// - `start` is bigger than blob size.
     /// - some portion of the range [start, start + size) is not covered by chunks.
     /// - the blob metadata is invalid.
-    pub fn get_chunks_uncompressed(&self, start: u64, size: u64) -> Result<Vec<BlobIoChunk>> {
+    pub fn get_chunks_uncompressed(
+        &self,
+        start: u64,
+        size: u64,
+        batch_size: u64,
+    ) -> Result<Vec<BlobIoChunk>> {
         let end = start.checked_add(size).ok_or_else(|| einval!())?;
         if end > self.state.uncompressed_size {
             return Err(einval!(format!(
@@ -413,6 +418,14 @@ impl BlobMetaInfo {
                 end, self.state.uncompressed_size
             )));
         }
+        let batch_end = if batch_size <= size {
+            end
+        } else {
+            std::cmp::min(
+                start.checked_add(batch_size).unwrap_or(end),
+                self.state.uncompressed_size,
+            )
+        };
 
         let infos = &*self.state.chunks;
         let mut index = self.state.get_chunk_index_nocheck(start, false)?;
@@ -431,7 +444,7 @@ impl BlobMetaInfo {
         vec.push(BlobMetaChunk::new(index, &self.state));
 
         let mut last_end = entry.aligned_uncompressed_end();
-        if last_end >= end {
+        if last_end >= batch_end {
             Ok(vec)
         } else {
             while index + 1 < infos.len() {
@@ -447,9 +460,14 @@ impl BlobMetaInfo {
                     )));
                 }
 
+                // Avoid read amplify if next chunk is too big.
+                if last_end >= end && entry.aligned_uncompressed_end() > batch_end {
+                    return Ok(vec);
+                }
+
                 vec.push(BlobMetaChunk::new(index, &self.state));
                 last_end = entry.aligned_uncompressed_end();
-                if last_end >= end {
+                if last_end >= batch_end {
                     return Ok(vec);
                 }
             }
@@ -469,7 +487,12 @@ impl BlobMetaInfo {
     /// - `start` is bigger than blob size.
     /// - some portion of the range [start, start + size) is not covered by chunks.
     /// - the blob metadata is invalid.
-    pub fn get_chunks_compressed(&self, start: u64, size: u64) -> Result<Vec<BlobIoChunk>> {
+    pub fn get_chunks_compressed(
+        &self,
+        start: u64,
+        size: u64,
+        batch_size: u64,
+    ) -> Result<Vec<BlobIoChunk>> {
         let end = start.checked_add(size).ok_or_else(|| einval!())?;
         if end > self.state.compressed_size {
             return Err(einval!(format!(
@@ -477,6 +500,14 @@ impl BlobMetaInfo {
                 end, self.state.compressed_size
             )));
         }
+        let batch_end = if batch_size <= size {
+            end
+        } else {
+            std::cmp::min(
+                start.checked_add(batch_size).unwrap_or(end),
+                self.state.uncompressed_size,
+            )
+        };
 
         let infos = &*self.state.chunks;
         let mut index = self.state.get_chunk_index_nocheck(start, true)?;
@@ -488,7 +519,7 @@ impl BlobMetaInfo {
         vec.push(BlobMetaChunk::new(index, &self.state));
 
         let mut last_end = entry.compressed_end();
-        if last_end >= end {
+        if last_end >= batch_end {
             Ok(vec)
         } else {
             while index + 1 < infos.len() {
@@ -499,9 +530,14 @@ impl BlobMetaInfo {
                     return Err(einval!());
                 }
 
+                // Avoid read amplify if next chunk is too big.
+                if last_end >= end && entry.compressed_end() > batch_end {
+                    return Ok(vec);
+                }
+
                 vec.push(BlobMetaChunk::new(index, &self.state));
                 last_end = entry.compressed_end();
-                if last_end >= end {
+                if last_end >= batch_end {
                     return Ok(vec);
                 }
             }
@@ -827,7 +863,7 @@ mod tests {
             state: Arc::new(state),
         };
 
-        let vec = info.get_chunks_uncompressed(0x0, 0x1001).unwrap();
+        let vec = info.get_chunks_uncompressed(0x0, 0x1001, 0).unwrap();
         assert_eq!(vec.len(), 1);
         assert_eq!(vec[0].blob_index(), 1);
         assert_eq!(vec[0].id(), 0);
@@ -838,7 +874,7 @@ mod tests {
         assert_eq!(vec[0].is_compressed(), true);
         assert_eq!(vec[0].is_hole(), false);
 
-        let vec = info.get_chunks_uncompressed(0x0, 0x4000).unwrap();
+        let vec = info.get_chunks_uncompressed(0x0, 0x4000, 0).unwrap();
         assert_eq!(vec.len(), 2);
         assert_eq!(vec[1].blob_index(), 1);
         assert_eq!(vec[1].id(), 1);
@@ -849,24 +885,24 @@ mod tests {
         assert_eq!(vec[1].is_compressed(), false);
         assert_eq!(vec[1].is_hole(), false);
 
-        let vec = info.get_chunks_uncompressed(0x0, 0x4001).unwrap();
+        let vec = info.get_chunks_uncompressed(0x0, 0x4001, 0).unwrap();
         assert_eq!(vec.len(), 3);
 
-        let vec = info.get_chunks_uncompressed(0x100000, 0x2000).unwrap();
+        let vec = info.get_chunks_uncompressed(0x100000, 0x2000, 0).unwrap();
         assert_eq!(vec.len(), 1);
 
-        assert!(info.get_chunks_uncompressed(0x0, 0x6001).is_err());
-        assert!(info.get_chunks_uncompressed(0x0, 0xfffff).is_err());
-        assert!(info.get_chunks_uncompressed(0x0, 0x100000).is_err());
-        assert!(info.get_chunks_uncompressed(0x0, 0x104000).is_err());
-        assert!(info.get_chunks_uncompressed(0x0, 0x104001).is_err());
-        assert!(info.get_chunks_uncompressed(0x100000, 0x2001).is_err());
-        assert!(info.get_chunks_uncompressed(0x100000, 0x4000).is_err());
-        assert!(info.get_chunks_uncompressed(0x100000, 0x4001).is_err());
+        assert!(info.get_chunks_uncompressed(0x0, 0x6001, 0).is_err());
+        assert!(info.get_chunks_uncompressed(0x0, 0xfffff, 0).is_err());
+        assert!(info.get_chunks_uncompressed(0x0, 0x100000, 0).is_err());
+        assert!(info.get_chunks_uncompressed(0x0, 0x104000, 0).is_err());
+        assert!(info.get_chunks_uncompressed(0x0, 0x104001, 0).is_err());
+        assert!(info.get_chunks_uncompressed(0x100000, 0x2001, 0).is_err());
+        assert!(info.get_chunks_uncompressed(0x100000, 0x4000, 0).is_err());
+        assert!(info.get_chunks_uncompressed(0x100000, 0x4001, 0).is_err());
         assert!(info
-            .get_chunks_uncompressed(0x102000, 0xffff_ffff_ffff_ffff)
+            .get_chunks_uncompressed(0x102000, 0xffff_ffff_ffff_ffff, 0)
             .is_err());
-        assert!(info.get_chunks_uncompressed(0x104000, 0x1).is_err());
+        assert!(info.get_chunks_uncompressed(0x104000, 0x1, 0).is_err());
     }
 
     #[test]

src/bin/nydusd/service_controller.rs
@@ -4,6 +4,7 @@
 
 use std::any::Any;
 use std::io::Result;
+#[cfg(target_os = "linux")]
 use std::path::Path;
 use std::sync::atomic::{AtomicBool, AtomicI32, Ordering};
 use std::sync::mpsc::{channel, Receiver, Sender};

storage/src/cache/cachedfile.rs
@@ -312,19 +312,15 @@ impl BlobObject for FileCacheEntry {
 
     fn fetch_range_compressed(&self, offset: u64, size: u64) -> Result<usize> {
         let meta = self.meta.as_ref().ok_or_else(|| einval!())?;
-        let chunks = meta.get_chunks_compressed(offset, size)?;
+        let chunks = meta.get_chunks_compressed(offset, size, RAFS_DEFAULT_CHUNK_SIZE * 2)?;
         debug_assert!(!chunks.is_empty());
-
         self.do_fetch_chunks(&chunks)
     }
 
     fn fetch_range_uncompressed(&self, offset: u64, size: u64) -> Result<usize> {
         let meta = self.meta.as_ref().ok_or_else(|| einval!())?;
-
-        // TODO: read amplify the range to naturally aligned 2M?
-        let chunks = meta.get_chunks_uncompressed(offset, size)?;
+        let chunks = meta.get_chunks_uncompressed(offset, size, RAFS_DEFAULT_CHUNK_SIZE * 2)?;
         debug_assert!(!chunks.is_empty());
-
         self.do_fetch_chunks(&chunks)
     }
 

commit 8aa4f30b7169e4f1efe8d01aec91b1648059aab9

storage: fix blob batch read error
When use batch_size option for get_chunks_uncompressed(), the
last chunks may still not reach the batch_end, then will return error.

Fixes: ab5e7c5fe8f1 ("storage: amplify blob read requests to enable batch operation")
Signed-off-by: Xin Yin <yinxin.x@bytedance.com>

storage/src/meta/mod.rs
@@ -489,6 +489,9 @@ impl BlobMetaInfo {
                 }
             }
 
+            if last_end > end {
+                return Ok(vec);
+            }
             Err(einval!(format!(
                 "entry not found index {} infos.len {}",
                 index,