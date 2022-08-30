dragonflyoss/nydus

commit a6521632ed8f8f7041978e0e8dfb1999791d6a95

rafs: provide open/opendir
To ask kernel to cache file and directory contents instead of dropping
them across open/close.

Signed-off-by: Peng Tao <bergwolf@hyper.sh>

rafs/src/fs.rs
@@ -664,6 +664,17 @@ impl FileSystem for Rafs {
         r
     }
 
+    fn open(
+        &self,
+        _ctx: &Context,
+        _inode: Self::Inode,
+        _flags: u32,
+        _fuse_flags: u32,
+    ) -> Result<(Option<Self::Handle>, OpenOptions)> {
+        // Keep cache since we are readonly
+        Ok((None, OpenOptions::KEEP_CACHE))
+    }
+
     fn release(
         &self,
         _ctx: &Context,
@@ -791,6 +802,16 @@ impl FileSystem for Rafs {
             rec.mark_success(0);
             r
         })
+    }
+
+    fn opendir(
+        &self,
+        _ctx: &Context,
+        _inode: Self::Inode,
+        _flags: u32,
+    ) -> Result<(Option<Self::Handle>, OpenOptions)> {
+        // Cache dir since we are readonly
+        Ok((None, OpenOptions::CACHE_DIR))
     }
 
     fn releasedir(&self, _ctx: &Context, _inode: u64, _flags: u32, _handle: u64) -> Result<()> {

commit 7c55d7f1e3868506ffc4fe8921a3349e38595aa1

rafs: fix the opendir() flag returned to kernel
Otherwise the directory cache will be dropped on opening by kernel [1].

https://github.com/torvalds/linux/blob/dcf8e5633e2e69ad60b730ab5905608b756a032f/fs/fuse/file.c#L263

Fixes: a6521632ed8f8f7041978e0e8dfb1999791d6a95
Reported-by: Daowen Luo <luodaowen.backend@bytedance.com>
Signed-off-by: Jiachen Zhang <zhangjiachen.jaycee@bytedance.com>

rafs/src/fs.rs
@@ -910,7 +910,7 @@ impl FileSystem for Rafs {
         _flags: u32,
     ) -> Result<(Option<Self::Handle>, OpenOptions)> {
         // Cache dir since we are readonly
-        Ok((None, OpenOptions::CACHE_DIR))
+        Ok((None, OpenOptions::CACHE_DIR | OpenOptions::KEEP_CACHE))
     }
 
     fn releasedir(&self, _ctx: &Context, _inode: u64, _flags: u32, _handle: u64) -> Result<()> {