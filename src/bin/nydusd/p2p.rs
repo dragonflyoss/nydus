a3cc04e94ae68a50c72bc3978423c9c50d68def7
Chenxi Jiang <chenxi.jiang.seu@gmail.com>
2022-10-24 21:01:37 +0800

    nydusd: support builtin P2P distribution

---
+++ src/bin/nydusd/p2p.rs
@@ -0,0 +1,7 @@
+// Copyright 2020 Ant Group. All rights reserved.
+// Copyright (C) 2020 Alibaba Cloud. All rights reserved.
+// Copyright 2019 Intel Corporation. All Rights Reserved.
+//
+// SPDX-License-Identifier: (Apache-2.0 AND BSD-3-Clause)
+
+mod dragonfly_rs;
