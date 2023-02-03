0d11f7cb6d6c9627c6ec81d74e19600c73b98536
Changwei Ge <gechangwei@bytedance.com>
2023-02-03 14:59:45 +0800

    test: let anolis smoke test apdat nydus-snapshotter's CLI
    Signed-off-by: Changwei Ge <gechangwei@bytedance.com>

--- tests/bats/common_tests.sh
+++ tests/bats/common_tests.sh
@@ -4,9 +4,9 @@ compile_image="localhost/compile-image:${rust_toolchain}"
 nydus_snapshotter_repo="https://github.com/containerd/nydus-snapshotter.git"
 
 run_nydus_snapshotter() {
-        rm -rf /var/lib/containerd/io.containerd.snapshotter.v1.nydus
-        rm -rf /var/lib/nydus/cache
-        cat > /tmp/nydus-erofs-config.json <<EOF
+  rm -rf /var/lib/containerd/io.containerd.snapshotter.v1.nydus
+  rm -rf /var/lib/nydus/cache
+  cat >/tmp/nydus-erofs-config.json <<EOF
 {
   "type": "bootstrap",
   "config": {
@@ -18,15 +18,15 @@ run_nydus_snapshotter() {
   }
 }
 EOF
-        containerd-nydus-grpc --config-path /tmp/nydus-erofs-config.json --daemon-mode shared \
-                --fs-driver fscache --root /var/lib/containerd/io.containerd.snapshotter.v1.nydus \
-                --address /run/containerd/containerd-nydus-grpc.sock --nydusd-path /usr/local/bin/nydusd \
-                --log-to-stdout > ${BATS_TEST_DIRNAME}/nydus-snapshotter-${BATS_TEST_NAME}.log 2>&1 &
+  containerd-nydus-grpc --config-path /tmp/nydus-erofs-config.json --daemon-mode shared \
+    --fs-driver fscache --root /var/lib/containerd/io.containerd.snapshotter.v1.nydus \
+    --address /run/containerd/containerd-nydus-grpc.sock --nydusd /usr/local/bin/nydusd \
+    --log-to-stdout >${BATS_TEST_DIRNAME}/nydus-snapshotter-${BATS_TEST_NAME}.log 2>&1 &
 }
 
 config_containerd_for_nydus() {
-        [ -d "/etc/containerd" ] || mkdir -p /etc/containerd
-        cat > /etc/containerd/config.toml <<EOF
+  [ -d "/etc/containerd" ] || mkdir -p /etc/containerd
+  cat >/etc/containerd/config.toml <<EOF
 version = 2
 
 [plugins]
@@ -46,5 +46,5 @@ version = 2
    snapshotter = "nydus"
    disable_snapshot_annotations = false
 EOF
-        systemctl restart containerd
+  systemctl restart containerd
 }
