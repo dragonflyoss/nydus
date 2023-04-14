dragonflyoss/nydus

commit 2616fb2c0502e4773a70a33184bdf014210ade77

rafs: fix a bug in calculate offset for dirent name
There's a bug in calculate offset and size for RAFS v6 Dirent name,
it will be treat as 0 instead of 4096 if the last block is 4096 bytes.

Fixes: https://github.com/dragonflyoss/image-service/issues/1098

Signed-off-by: Jiang Liu <gerry@linux.alibaba.com>

rafs/src/metadata/direct_v6.rs
@@ -408,7 +408,13 @@ impl OndiskInodeWrapper {
                 let block_count = self.blocks_count() as usize;
                 let len = match block_count.cmp(&(block_index + 1)) {
                     Ordering::Greater => (EROFS_BLOCK_SIZE - base) as usize,
-                    Ordering::Equal => (self.size() % EROFS_BLOCK_SIZE - base) as usize,
+                    Ordering::Equal => {
+                        if self.size() % EROFS_BLOCK_SIZE == 0 {
+                            EROFS_BLOCK_SIZE as usize
+                        } else {
+                            (self.size() % EROFS_BLOCK_SIZE - base) as usize
+                        }
+                    }
                     Ordering::Less => return Err(RafsError::InvalidImageData),
                 };
 

commit 56c48bcccb1290a39f237a73a0f08d1cf6381f9c

rafs: fix a regression caused by commit 2616fb2c0502e4
Fix a regression caused by commit 2616fb2c0502e4.

Signed-off-by: Jiang Liu <gerry@linux.alibaba.com>

rafs/src/metadata/direct_v6.rs
@@ -439,7 +439,7 @@ impl OndiskInodeWrapper {
                     Ordering::Greater => (block_size - base) as usize,
                     Ordering::Equal => {
                         if self.size() % block_size == 0 {
-                            block_size as usize
+                            (block_size - base) as usize
                         } else {
                             (self.size() % block_size - base) as usize
                         }