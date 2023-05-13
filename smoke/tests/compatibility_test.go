23c61104a988fa803d2f0890466e119a41e2e84a
Jiang Liu <gerry@linux.alibaba.com>
2023-05-14 00:07:50 +0800

    smoke: use v2.1.6 instead of v2.1.4
    Update smoke tests to use the latest v2.1.6 instead of v2.1.4.
    
    Signed-off-by: Jiang Liu <gerry@linux.alibaba.com>

--- smoke/tests/compatibility_test.go
+++ smoke/tests/compatibility_test.go
@@ -29,9 +29,9 @@ func (c *CompatibilityTestSuite) TestConvertImages() test.Generator {
 	scenarios.
 		Dimension(paramImage, []interface{}{"nginx:latest"}).
 		Dimension(paramFSVersion, []interface{}{"5", "6"}).
-		Dimension(paramNydusImageVersion, []interface{}{"v0.1.0", "v2.1.4", "latest"}).
-		Dimension(paramNydusifyVersion, []interface{}{"v0.1.0", "v2.1.4", "latest"}).
-		Dimension(paramNydusdVersion, []interface{}{"v0.1.0", "v2.1.4", "latest"}).
+		Dimension(paramNydusImageVersion, []interface{}{"v0.1.0", "v2.1.6", "latest"}).
+		Dimension(paramNydusifyVersion, []interface{}{"v0.1.0", "v2.1.6", "latest"}).
+		Dimension(paramNydusdVersion, []interface{}{"v0.1.0", "v2.1.6", "latest"}).
 		Skip(func(param *tool.DescartesItem) bool {
 
 			// Nydus-image 0.1.0 only works with nydus-nydusify 0.1.0, vice versa.
