9bb51517beb978c50f2cc817b0c551888fbd413a
泰友 <cuichengxu.ccx@antgroup.com>
2023-06-25 14:15:49 +0800

    fix: deprecated docker field leads to failure of nydusify check
    `NydusImage.Config.Config.ArgsEscaped` is present only for legacy compatibility
    with Docker and should not be used by new image builders. Nydusify (1.6 and
    above) ignores it, which is an expected behavior.
    
    This pr ignores comparision of it in nydusify checking, which leads to failure.
    
    Signed-off-by: 泰友 <cuichengxu.ccx@antgroup.com>

---
+++ contrib/nydusify/pkg/checker/rule/manifest_test.go
@@ -0,0 +1,38 @@
+package rule
+
+import (
+	"testing"
+
+	"github.com/dragonflyoss/image-service/contrib/nydusify/pkg/parser"
+	"github.com/stretchr/testify/assert"
+
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
+func TestManifestRuleValidate_IgnoreDeprecatedField(t *testing.T) {
+	source := &parser.Parsed{
+		NydusImage: &parser.Image{
+			Config: v1.Image{
+				Config: v1.ImageConfig{
+					ArgsEscaped: true, // deprecated field
+				},
+			},
+		},
+	}
+	target := &parser.Parsed{
+		NydusImage: &parser.Image{
+			Config: v1.Image{
+				Config: v1.ImageConfig{
+					ArgsEscaped: false,
+				},
+			},
+		},
+	}
+
+	rule := ManifestRule{
+		SourceParsed: source,
+		TargetParsed: target,
+	}
+
+	assert.Nil(t, rule.Validate())
+}
