fc86651d6d0501f11159063f0a53b7bb79e3c8bf
Nan Li <loheagn@icloud.com>
2023-09-26 09:26:02 +0000

    feat: implement `takeover` for nydusd fusedev daemon
    This patch implements the `save` and `restore` functions in the `fusedev_upgrade` in the service create.
    To do this,
    - This patch add a new create named `nydus-upgrade` into the workspace. The `nydus-upgrade` create has some util functions help to do serialization and deserialization for the rust structs using the versionize and snapshot crates. The crate also has a trait named `StorageBackend` which can be used to store and restore fuse session fds and state data for the upgrade action, and there's also an implementation named `UdsStorageBackend` which uses unix domain socket to do this.
    - as we have to use the same fuse session connection, backend file system mount commands, Vfs to re-mount the rafs for the new daemon (created for "hot upgrade" or failover), this patch add a new struct named `FusedevState` to hold these information. The `FusedevState` is serialized and stored into the `UdsStorageBackend` (which happens in the `save` function in the `fusedev_upgrade` module) before the new daemon is created, and the `FusedevState` is deserialized and restored from the `UdsStorageBackend` (which happens in the `restore` function in the `fusedev_upgrade` module) when the new daemon is triggered by `takeover`.
    
    Signed-off-by: Nan Li <loheagn@icloud.com>
    Signed-off-by: linan.loheagn3 <linan.loheagn3@bytedance.com>

--- api/src/http.rs
+++ api/src/http.rs
@@ -132,7 +132,7 @@ pub enum DaemonErrorKind {
     /// Unexpected event type.
     UnexpectedEvent(String),
     /// Can't upgrade the daemon.
-    UpgradeManager,
+    UpgradeManager(String),
     /// Unsupported requests.
     Unsupported,
 }
--- service/src/upgrade.rs
+++ service/src/upgrade.rs
@@ -4,15 +4,40 @@
 
 //! Online upgrade manager for Nydus daemons and filesystems.
 
+use std::any::TypeId;
+use std::collections::HashMap;
 use std::convert::{TryFrom, TryInto};
+use std::io;
+use std::os::fd::RawFd;
 use std::path::PathBuf;
+use std::sync::atomic::{AtomicBool, Ordering};
+
+use nydus_upgrade::backend::unix_domain_socket::UdsStorageBackend;
+use nydus_upgrade::backend::{StorageBackend, StorageBackendErr};
 
 use crate::fs_service::{FsBackendMountCmd, FsBackendUmountCmd};
-use crate::Result;
+use crate::{Error, Result};
 
 /// Error codes related to upgrade manager.
 #[derive(thiserror::Error, Debug)]
-pub enum UpgradeMgrError {}
+pub enum UpgradeMgrError {
+    #[error("missing supervisor path")]
+    MissingSupervisorPath,
+
+    #[error("failed to save/restore data via the backend, {0}")]
+    StorageBackendError(StorageBackendErr),
+
+    #[error("failed to serialize, {0}")]
+    Serialize(io::Error),
+    #[error("failed to deserialize, {0}")]
+    Deserialize(io::Error),
+}
+
+impl From<UpgradeMgrError> for Error {
+    fn from(e: UpgradeMgrError) -> Self {
+        Error::UpgradeManager(e)
+    }
+}
 
 /// FUSE fail-over policies.
 #[derive(PartialEq, Eq, Debug)]
@@ -43,46 +68,255 @@ impl TryFrom<&String> for FailoverPolicy {
     }
 }
 
+const MAX_STATE_DATA_LENGTH: usize = 1024 * 32;
+
 /// Online upgrade manager.
-pub struct UpgradeManager {}
+pub struct UpgradeManager {
+    // backend_mount_cmd_map records the mount command of each backend filesystem.
+    // the structure is: mountpoint -> (FsBackendMountCmd, vfs_index)
+    backend_mount_cmd_map: HashMap<String, (FsBackendMountCmd, u8)>,
+    fds: Vec<RawFd>,
+    backend: Box<dyn StorageBackend>,
+
+    disabled: AtomicBool,
+}
 
 impl UpgradeManager {
     /// Create a new instance of [UpgradeManager].
-    pub fn new(_: PathBuf) -> Self {
-        UpgradeManager {}
+    pub fn new(socket_path: PathBuf) -> Self {
+        UpgradeManager {
+            backend_mount_cmd_map: HashMap::new(),
+            backend: Box::new(UdsStorageBackend::new(socket_path)),
+            fds: Vec::new(),
+            disabled: AtomicBool::new(false),
+        }
     }
 
     /// Add a filesystem instance into the upgrade manager.
-    pub fn add_mounts_state(&mut self, _cmd: FsBackendMountCmd, _vfs_index: u8) -> Result<()> {
+    pub fn add_mounts_state(&mut self, cmd: FsBackendMountCmd, vfs_index: u8) -> Result<()> {
+        if self.disabled.load(Ordering::Acquire) {
+            return Err(Error::Unsupported);
+        }
+
+        let cmd_map = &mut self.backend_mount_cmd_map;
+        if cmd_map.contains_key(&cmd.mountpoint) {
+            return Err(Error::AlreadyExists);
+        }
+
+        cmd_map.insert(cmd.mountpoint.clone(), (cmd, vfs_index));
         Ok(())
     }
 
     /// Update a filesystem instance in the upgrade manager.
-    pub fn update_mounts_state(&mut self, _cmd: FsBackendMountCmd) -> Result<()> {
-        Ok(())
+    pub fn update_mounts_state(&mut self, cmd: FsBackendMountCmd) -> Result<()> {
+        if self.disabled.load(Ordering::Acquire) {
+            return Err(Error::Unsupported);
+        }
+
+        let cmd_map = &mut self.backend_mount_cmd_map;
+        match cmd_map.get_mut(&cmd.mountpoint) {
+            Some(cmd_with_vfs_idx) => {
+                cmd_with_vfs_idx.0 = cmd;
+                Ok(())
+            }
+            None => Err(Error::NotFound),
+        }
     }
 
     /// Remove a filesystem instance from the upgrade manager.
-    pub fn remove_mounts_state(&mut self, _cmd: FsBackendUmountCmd) -> Result<()> {
-        Ok(())
+    pub fn remove_mounts_state(&mut self, cmd: FsBackendUmountCmd) -> Result<()> {
+        if self.disabled.load(Ordering::Acquire) {
+            return Err(Error::Unsupported);
+        }
+
+        let cmd_map = &mut self.backend_mount_cmd_map;
+        match cmd_map.get_mut(&cmd.mountpoint) {
+            Some(_) => {
+                cmd_map.remove(&cmd.mountpoint);
+                Ok(())
+            }
+            None => Err(Error::NotFound),
+        }
     }
 
     /// Disable online upgrade capability.
-    pub fn disable_upgrade(&mut self) {}
+    pub fn disable_upgrade(&mut self) {
+        self.disabled.store(true, Ordering::Release);
+    }
+
+    /// Save the fuse fds and fuse state data for online upgrade.
+    fn save(&mut self, data: &Vec<u8>) -> Result<()> {
+        self.backend
+            .save(&self.fds, data)
+            .map_err(UpgradeMgrError::StorageBackendError)?;
+        Ok(())
+    }
+
+    /// Restore the fuse fds and fuse state data for online upgrade.
+    fn restore(&mut self) -> Result<Vec<u8>> {
+        let mut fds = vec![0 as RawFd; 8];
+        let mut state_data = vec![0u8; MAX_STATE_DATA_LENGTH];
+        let (state_data_len, fds_len) = self
+            .backend
+            .restore(&mut fds, &mut state_data)
+            .map_err(UpgradeMgrError::StorageBackendError)?;
+        fds.truncate(fds_len);
+        state_data.truncate(state_data_len);
+
+        self.fds = fds;
+        Ok(state_data)
+    }
+
+    fn add_fd(&mut self, fd: RawFd) {
+        self.fds.push(fd);
+    }
 }
 
 /// Online upgrade utilities for FUSE daemon.
 pub mod fusedev_upgrade {
+    use std::fs::File;
+    use std::os::fd::{FromRawFd, RawFd};
+    use std::os::unix::io::AsRawFd;
+    use std::sync::atomic::Ordering;
+
+    use nydus_upgrade::persist::Snapshotter;
+    use versionize::{VersionMap, Versionize, VersionizeResult};
+    use versionize_derive::Versionize;
+
     use super::*;
-    use crate::fusedev::FusedevDaemon;
+    use crate::daemon::NydusDaemon;
+    use crate::fusedev::{FusedevDaemon, FusedevFsService};
+
+    #[derive(Versionize, Clone, Default)]
+    struct FusedevState {
+        backend_fs_mount_cmd_list: Vec<(FsBackendMountCmd, u8)>,
+        vfs_state_data: Vec<u8>,
+        fuse_conn_id: u64,
+    }
+
+    impl Snapshotter for FusedevState {
+        fn get_versions() -> Vec<HashMap<TypeId, u16>> {
+            vec![
+                // version 1
+                HashMap::from([(FusedevState::type_id(), 1)]),
+                // more versions for the future
+            ]
+        }
+    }
 
     /// Save state information for a FUSE daemon.
-    pub fn save(_daemon: &FusedevDaemon) -> Result<()> {
+    pub fn save(daemon: &FusedevDaemon) -> Result<()> {
+        let svc = daemon.get_default_fs_service().ok_or(Error::NotFound)?;
+
+        if !svc.get_vfs().initialized() {
+            return Err(Error::NotReady);
+        }
+
+        let mut mgr = svc.upgrade_mgr().unwrap();
+
+        // set fd
+        let fd = svc
+            .as_ref()
+            .as_any()
+            .downcast_ref::<FusedevFsService>()
+            .unwrap()
+            .session
+            .lock()
+            .unwrap()
+            .get_fuse_file()
+            .unwrap()
+            .as_raw_fd();
+        mgr.add_fd(fd);
+
+        // save vfs state
+        let vfs_state_data = svc.get_vfs().save_to_bytes().map_err(|e| {
+            let io_err = io::Error::new(
+                io::ErrorKind::Other,
+                format!("Failed to save vfs state: {:?}", e),
+            );
+            UpgradeMgrError::Serialize(io_err)
+        })?;
+
+        let backend_fs_mount_cmd_list = mgr
+            .backend_mount_cmd_map
+            .iter()
+            .map(|(_, (cmd, vfs_idx))| (cmd.clone(), *vfs_idx))
+            .collect();
+
+        let fuse_conn_id = svc
+            .as_any()
+            .downcast_ref::<FusedevFsService>()
+            .unwrap()
+            .conn
+            .load(Ordering::Acquire);
+
+        let state = FusedevState {
+            backend_fs_mount_cmd_list,
+            vfs_state_data,
+            fuse_conn_id,
+        };
+        let state = state.save().map_err(UpgradeMgrError::Serialize)?;
+
+        // save the mgr state via the backend in the mgr
+        mgr.save(&state)?;
+
         Ok(())
     }
 
     /// Restore state information for a FUSE daemon.
-    pub fn restore(_daemon: &FusedevDaemon) -> Result<()> {
+    pub fn restore(daemon: &FusedevDaemon) -> Result<()> {
+        if daemon.supervisor.is_none() {
+            return Err(UpgradeMgrError::MissingSupervisorPath.into());
+        }
+
+        let svc = daemon.get_default_fs_service().ok_or(Error::NotFound)?;
+
+        let mut mgr = svc.upgrade_mgr().unwrap();
+
+        // restore the mgr state via the backend in the mgr
+        let mut state_data = mgr.restore()?;
+
+        let mut state =
+            FusedevState::restore(&mut state_data).map_err(UpgradeMgrError::Deserialize)?;
+
+        // restore the backend fs mount cmd map
+        state
+            .backend_fs_mount_cmd_list
+            .iter()
+            .for_each(|(cmd, vfs_idx)| {
+                mgr.backend_mount_cmd_map
+                    .insert(cmd.mountpoint.clone(), (cmd.clone(), *vfs_idx));
+            });
+
+        // restore the fuse daemon
+        svc.as_any()
+            .downcast_ref::<FusedevFsService>()
+            .unwrap()
+            .conn
+            .store(state.fuse_conn_id, Ordering::Release);
+
+        // restore fuse fd
+        svc.as_any()
+            .downcast_ref::<FusedevFsService>()
+            .unwrap()
+            .session
+            .lock()
+            .unwrap()
+            .set_fuse_file(unsafe { File::from_raw_fd(mgr.fds[0] as RawFd) });
+
+        // restore vfs
+        svc.get_vfs()
+            .restore_from_bytes(&mut state.vfs_state_data)?;
+        state
+            .backend_fs_mount_cmd_list
+            .iter()
+            .try_for_each(|(cmd, vfs_idx)| -> Result<()> {
+                svc.restore_mount(cmd, *vfs_idx)?;
+                // as we are in upgrade stage and obtain the lock, `unwrap` is safe here
+                mgr.add_mounts_state(cmd.clone(), *vfs_idx).unwrap();
+                Ok(())
+            })?;
+
         Ok(())
     }
 }
