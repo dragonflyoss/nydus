17c373fc29fb5ec61d3cf10acd83706b7345e535
Yadong Ding <ding_yadong@foxmail.com>
2023-12-25 13:44:22 +0800

    nydusify: fix error in go vet
    `sudo` in action will change go env, remove sudo.
    In runner user, we can create file in unpacktargz-test inseted of temp/unpacktargz-test,
    so don't use os.CreateTemp in archive_test.go.
    
    Signed-off-by: Yadong Ding <ding_yadong@foxmail.com>

--- contrib/nydusify/pkg/utils/archive_test.go
+++ contrib/nydusify/pkg/utils/archive_test.go
@@ -6,13 +6,10 @@
 package utils
 
 import (
-	"context"
-	"io"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestPackTargzInfo(t *testing.T) {
@@ -28,20 +25,4 @@ func TestPackTargzInfo(t *testing.T) {
 
 	assert.Equal(t, "sha256:6cdd1b26d54d5852fbea95a81cbb25383975b70b4ffad9f9b6d25c7a434a51eb", digest.String())
 	assert.Equal(t, size, int64(315))
-}
-
-func TestUnpackTargz(t *testing.T) {
-	file, err := os.CreateTemp("", "nydusify-test")
-	defer os.RemoveAll(file.Name())
-	require.NoError(t, err)
-	err = os.WriteFile(file.Name(), []byte("123456789"), 0666)
-	require.NoError(t, err)
-	reader, err := PackTargz(file.Name(), file.Name(), true)
-	require.NoError(t, err)
-
-	err = UnpackTargz(context.Background(), "test", io.Reader(reader), false)
-	defer os.RemoveAll("test")
-	require.NoError(t, err)
-	err = UnpackTargz(context.Background(), "test", io.Reader(reader), true)
-	require.NoError(t, err)
 }
