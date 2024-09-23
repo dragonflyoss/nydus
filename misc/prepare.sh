b675ef1e40703ecbb79a46a260c68f37edd576aa
Yadong Ding <ding_yadong@foxmail.com>
2024-09-23 16:45:14 +0800

    chore: keep smoke test component latest version
    - Use the latest `nerdctl`, `nydus-snapshotter`, and `cni` in smoke test env.
    - Delete `misc/takeover/snapshotter_config.toml`, use modifyed `misc/performance/snapshotter_config.toml` when test.
    
    Signed-off-by: Yadong Ding <ding_yadong@foxmail.com>

--- misc/prepare.sh
+++ misc/prepare.sh
@@ -2,24 +2,24 @@
 
 SNAPSHOTTER_CONFIG="misc/performance/snapshotter_config.toml"
 if [ "$1" == "takeover_test" ]; then
-    SNAPSHOTTER_CONFIG="misc/takeover/snapshotter_config.toml"
+    sed -i 's/recover_policy = "restart"/recover_policy = "failover"/' "$SNAPSHOTTER_CONFIG"
 fi
 
-readonly SNAPSHOTTER_VERSION=0.14.0
-readonly NERDCTL_VERSION=1.7.6
-readonly CNI_PLUGINS_VERSION=1.5.0
+readonly SNAPSHOTTER_VERSION=`curl -s https://api.github.com/repos/containerd/nydus-snapshotter/releases/latest | grep tag_name | cut -f4 -d "\""`
+readonly NERDCTL_VERSION=`curl -s https://api.github.com/repos/containerd/nerdctl/releases/latest | grep tag_name | cut -f4 -d "\"" | sed 's/^v//g'`
+readonly CNI_PLUGINS_VERSION=`curl -s https://api.github.com/repos/containernetworking/plugins/releases/latest | grep tag_name | cut -f4 -d "\""`
 
 # setup nerdctl and nydusd env
 sudo install -D -m 755 contrib/nydusify/cmd/nydusify /usr/local/bin
 sudo install -D -m 755 target/release/nydusd target/release/nydus-image /usr/local/bin
-wget https://github.com/containerd/nydus-snapshotter/releases/download/v$SNAPSHOTTER_VERSION/nydus-snapshotter-v$SNAPSHOTTER_VERSION-linux-amd64.tar.gz
-tar zxvf nydus-snapshotter-v$SNAPSHOTTER_VERSION-linux-amd64.tar.gz
+wget https://github.com/containerd/nydus-snapshotter/releases/download/$SNAPSHOTTER_VERSION/nydus-snapshotter-$SNAPSHOTTER_VERSION-linux-amd64.tar.gz
+tar zxvf nydus-snapshotter-$SNAPSHOTTER_VERSION-linux-amd64.tar.gz
 sudo install -D -m 755 bin/containerd-nydus-grpc /usr/local/bin
 sudo wget https://github.com/containerd/nerdctl/releases/download/v$NERDCTL_VERSION/nerdctl-$NERDCTL_VERSION-linux-amd64.tar.gz
 sudo tar -xzvf nerdctl-$NERDCTL_VERSION-linux-amd64.tar.gz -C /usr/local/bin
 sudo mkdir -p /opt/cni/bin
-sudo wget https://github.com/containernetworking/plugins/releases/download/v$CNI_PLUGINS_VERSION/cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz 
-sudo tar -xzvf cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz -C /opt/cni/bin
+sudo wget https://github.com/containernetworking/plugins/releases/download/$CNI_PLUGINS_VERSION/cni-plugins-linux-amd64-$CNI_PLUGINS_VERSION.tgz 
+sudo tar -xzvf cni-plugins-linux-amd64-$CNI_PLUGINS_VERSION.tgz -C /opt/cni/bin
 sudo install -D misc/performance/containerd_config.toml /etc/containerd/config.toml
 sudo systemctl restart containerd
 sudo install -D misc/performance/nydusd_config.json /etc/nydus/nydusd-config.fusedev.json
