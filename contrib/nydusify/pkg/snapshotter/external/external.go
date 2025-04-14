9b915529a938acc3d84cdd230e3333bef6975194
Yang Kaiyong <yangkaiyong.yky@antgroup.com>
2025-04-14 16:06:54 +0800

    nydusify(feat): add crc32 in file attributes
    Read CRC32 from external models' manifest and pass it to builder.
    
    Signed-off-by: Yang Kaiyong <yangkaiyong.yky@antgroup.com>

--- contrib/nydusify/pkg/snapshotter/external/external.go
+++ contrib/nydusify/pkg/snapshotter/external/external.go
@@ -90,8 +90,8 @@ func RemoteHandle(ctx context.Context, opts Options) error {
 	}
 	attributes := []Attribute{}
 	for _, file := range fileAttrs {
-		p := fmt.Sprintf("/%s type=%s file_size=%d blob_index=%d blob_id=%s chunk_size=%s chunk_0_compressed_offset=%d compressed_size=%s",
-			file.RelativePath, file.Type, file.FileSize, file.BlobIndex, file.BlobID, file.ChunkSize, file.Chunk0CompressedOffset, file.BlobSize)
+		p := fmt.Sprintf("/%s type=%s file_size=%d blob_index=%d blob_id=%s chunk_size=%s chunk_0_compressed_offset=%d compressed_size=%s crcs=%s",
+			file.RelativePath, file.Type, file.FileSize, file.BlobIndex, file.BlobID, file.ChunkSize, file.Chunk0CompressedOffset, file.BlobSize, file.Crcs)
 		attributes = append(attributes, Attribute{
 			Pattern: p,
 		})
